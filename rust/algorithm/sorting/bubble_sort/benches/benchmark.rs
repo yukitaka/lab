4e56bd1bc39ae1416f41a076f6453f02315e7174
Takayuki Sato <s.yukkiy@gmail.com>
2022-01-02 15:46:53 +0900

    feat: Benchmark improvement

--- rust/algorithm/sorting/bubble_sort/benches/benchmark.rs
+++ rust/algorithm/sorting/bubble_sort/benches/benchmark.rs
@@ -1,22 +1,30 @@
 use criterion::{criterion_group, criterion_main, Criterion};
+use rand::{ Rng, SeedableRng };
 
 use bslib::bubble_sort_immutable;
 use bslib::bubble_sort_mutable;
 
-fn bm1(c: &mut Criterion) {
-    c.bench_function("Bubble Sort - Immutable", |b| {
-        b.iter(|| bubble_sort_immutable(vec![3, 7, 4, 9, 1, 0]))
+fn numbers() -> Vec<i32> {
+    let mut rng = rand::rngs::SmallRng::seed_from_u64(100);
+    let mut numbers = vec![rng.gen()];
+    for _ in 0..=100 {
+        numbers.push(rng.gen());
+    }
+
+    numbers
+}
+
+fn bench(c: &mut Criterion) {
+    let mut group = c.benchmark_group("Bubble Sort");
+
+    let nums = numbers();
+    group.bench_function("Immutable", |b| {
+        b.iter(|| bubble_sort_immutable(nums.clone()))
+    });
+    group.bench_function("Mutable", |b| {
+        b.iter(|| bubble_sort_mutable(&mut nums.clone()))
     });
 }
 
-fn bm2(c: &mut Criterion) {
-    c.bench_function("Bubble Sort - Mutable", |b| {
-        b.iter(|| {
-            let mut numbers = vec![3, 7, 4, 9, 1, 0];
-            bubble_sort_mutable(&mut numbers)
-        })
-    });
-}
-
-criterion_group!(benches, bm1, bm2);
+criterion_group!(benches, bench);
 criterion_main!(benches);
