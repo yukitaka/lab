e5c7de4673cb637c3932cc453bbca2024e2a29c4
Takayuki Sato <s.yukkiy@gmail.com>
2022-01-03 20:05:46 +0900

    feat: Add benchmark

---
+++ rust/algorithm/sorting/comb_sort/benches/benchmark.rs
@@ -0,0 +1,22 @@
+use criterion::{criterion_group, criterion_main, Criterion};
+use rand::{Rng, SeedableRng};
+
+use cslib::comb_sort;
+
+fn numbers() -> Vec<i32> {
+    let mut rng = rand::rngs::SmallRng::seed_from_u64(100);
+    let mut numbers = vec![rng.gen()];
+    for _ in 0..=100 {
+        numbers.push(rng.gen());
+    }
+
+    numbers
+}
+
+fn bench(c: &mut Criterion) {
+    let nums = numbers();
+    c.bench_function("Comb sort", |b| b.iter(|| comb_sort(&mut nums.clone())));
+}
+
+criterion_group!(benches, bench);
+criterion_main!(benches);
