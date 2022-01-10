68a38d52df23d8c31beebc81d2d46b988945718c
Takayuki Sato <s.yukkiy@gmail.com>
2022-01-10 12:50:20 +0900

    feat: The Rust Programming Language 20-25

--- rust/web/example/src/main.rs
+++ rust/web/example/src/main.rs
@@ -7,15 +7,16 @@ use std::time::Duration;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
+    let pool = ThreadPool::new(4);
 
-    for stream in listener.incoming() {
+    for stream in listener.incoming().take(2) {
         let stream = stream.unwrap();
-        let pool = ThreadPool::new(4);
 
         pool.execute(|| {
             handle_connection(stream);
         });
     }
+    println!("Shutting down.");
 }
 
 fn handle_connection(mut stream: TcpStream) {
