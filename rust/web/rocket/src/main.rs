64e77f5cd446a88eeadf6d2195310170f86e8fe5
Takayuki Sato <s.yukkiy@gmail.com>
2022-01-11 22:53:43 +0900

    feat: Add test

--- rust/web/rocket/src/main.rs
+++ rust/web/rocket/src/main.rs
@@ -1,4 +1,5 @@
-#[macro_use] extern crate rocket;
+#[macro_use]
+extern crate rocket;
 
 #[get("/")]
 fn index() -> &'static str {
@@ -8,4 +9,19 @@ fn index() -> &'static str {
 #[launch]
 fn rocket() -> _ {
     rocket::build().mount("/", routes![index])
+}
+
+#[cfg(test)]
+mod test {
+    use super::rocket;
+    use rocket::http::Status;
+    use rocket::local::blocking::Client;
+
+    #[test]
+    fn hello_world() {
+        let client = Client::tracked(rocket()).expect("valid rocket instance");
+        let response = client.get("/").dispatch();
+        assert_eq!(Status::Ok, response.status());
+        assert_eq!("Hello, world!", response.into_string().unwrap());
+    }
 }
