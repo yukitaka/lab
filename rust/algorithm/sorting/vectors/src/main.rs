4e89214b37fc149d340639f8ef2cdb15e4ffcf8a
Takayuki Sato <s.yukkiy@gmail.com>
2022-01-15 18:06:32 +0900

    feat: Add sort a vector of structs

--- rust/algorithm/sorting/vectors/src/main.rs
+++ rust/algorithm/sorting/vectors/src/main.rs
@@ -1,6 +1,7 @@
 fn main() {
     sort_a_vector_of_integers();
     sort_a_vector_of_floats();
+    sort_a_vector_of_structs();
 }
 
 fn sort_a_vector_of_integers() {
@@ -15,4 +16,44 @@ fn sort_a_vector_of_floats() {
     vec.sort_by(|a, b| a.partial_cmp(b).unwrap());
 
     assert_eq!(vec, vec![1.1, 1.123, 1.15, 2.0, 5.5]);
+}
+
+#[derive(Debug, Eq, Ord, PartialEq, PartialOrd)]
+struct Person {
+    name: String,
+    age: u32,
+}
+
+impl Person {
+    fn new(name: String, age: u32) -> Self {
+        Person { name, age }
+    }
+}
+
+fn sort_a_vector_of_structs() {
+    let mut people = vec![
+        Person::new("Zoe".to_string(), 25),
+        Person::new("Al".to_string(), 60),
+        Person::new("John".to_string(), 1),
+    ];
+
+    people.sort();
+    assert_eq!(
+        people,
+        vec![
+            Person::new("Al".to_string(), 60),
+            Person::new("John".to_string(), 1),
+            Person::new("Zoe".to_string(), 25),
+        ]
+    );
+
+    people.sort_by(|a, b| b.age.cmp(&a.age));
+    assert_eq!(
+        people,
+        vec![
+            Person::new("Al".to_string(), 60),
+            Person::new("Zoe".to_string(), 25),
+            Person::new("John".to_string(), 1),
+        ]
+    );
 }
