b7f8b1b7a4046375d21a5d1e575404cb7daf7ee0
Takayuki Sato <s.yukkiy@gmail.com>
2022-01-17 00:24:27 +0900

    feat: Add compress a directory into tarball

--- rust/compression/tarball/src/main.rs
+++ rust/compression/tarball/src/main.rs
@@ -1,10 +1,11 @@
 fn main() {
     let _ = decompress_a_tarball();
+    let _ = compress_a_directory_into_tarball();
 }
 
 fn decompress_a_tarball() -> Result<(), std::io::Error> {
-    use std::fs::File;
     use flate2::read::GzDecoder;
+    use std::fs::File;
     use tar::Archive;
 
     let path = "archive.tar.gz";
@@ -14,5 +15,17 @@ fn decompress_a_tarball() -> Result<(), std::io::Error> {
     let mut archive = Archive::new(tar);
     archive.unpack("./dst")?;
 
+    Ok(())
+}
+
+fn compress_a_directory_into_tarball() -> Result<(), std::io::Error> {
+    use flate2::write::GzEncoder;
+    use flate2::Compression;
+    use std::fs::File;
+
+    let tar_gz = File::create("archive.tar.gz")?;
+    let enc = GzEncoder::new(tar_gz, Compression::default());
+    let mut tar = tar::Builder::new(enc);
+    tar.append_dir_all("backup/logs", "/var/log")?;
     Ok(())
 }
