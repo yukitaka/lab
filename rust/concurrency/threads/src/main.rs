1715d90f17f6782b1c34cf6a060c32fd8bdcf37a
Takayuki Sato <s.yukkiy@gmail.com>
2022-01-18 00:00:51 +0900

    feat: Add pass data between two threads

--- rust/concurrency/threads/src/main.rs
+++ rust/concurrency/threads/src/main.rs
@@ -3,6 +3,7 @@ mod parallel_pipeline;
 fn main() {
     spawn_a_short_lived_thread();
     parallel_pipeline::create_a_parallel_pipeline();
+    pass_data_between_two_threads();
 }
 
 fn spawn_a_short_lived_thread() {
@@ -31,4 +32,26 @@ fn find_max(arr: &[i32]) -> Option<i32> {
         Some(max_l.max(max_r))
     })
     .unwrap()
+}
+
+fn pass_data_between_two_threads() {
+    use crossbeam_channel::unbounded;
+    use std::{thread, time};
+
+    println!("Pass data between two threads");
+    let (snd, rcv) = unbounded();
+    let n_msgs = 5;
+    crossbeam::scope(|s| {
+        s.spawn(|_| {
+            for i in 0..n_msgs {
+                snd.send(i).unwrap();
+                thread::sleep(time::Duration::from_millis(100));
+            }
+        });
+    })
+    .unwrap();
+    for _ in 0..n_msgs {
+        let msg = rcv.recv().unwrap();
+        println!("Received {}", msg);
+    }
 }
