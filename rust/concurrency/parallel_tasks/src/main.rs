27a936dcbf657721d3c4c3013cd9dcff6b54f80d
Takayuki Sato <s.yukkiy@gmail.com>
2022-01-19 00:52:12 +0900

    feat: Add search items using given predicate in parallel

--- rust/concurrency/parallel_tasks/src/main.rs
+++ rust/concurrency/parallel_tasks/src/main.rs
@@ -3,6 +3,7 @@ use rayon::prelude::*;
 fn main() {
     mutate_the_elements_of_an_array_in_parallel();
     test_in_parallel_if_any_or_all_elements_of_a_collection_match_a_given_predicate();
+    search_items_using_given_predicate_in_parallel();
 }
 
 fn mutate_the_elements_of_an_array_in_parallel() {
@@ -27,4 +28,16 @@ fn test_in_parallel_if_any_or_all_elements_of_a_collection_match_a_given_predica
     assert!(!vec.par_iter().all(|n| *n <= 8));
 
     println!("{:?}", vec);
+}
+
+fn search_items_using_given_predicate_in_parallel() {
+    let v = vec![6, 2, 1, 9, 3, 8, 11];
+
+    let f1 = v.par_iter().find_any(|&&x| x == 9);
+    let f2 = v.par_iter().find_any(|&&x| x % 2 == 0 && x > 6);
+    let f3 = v.par_iter().find_any(|&&x| x > 8);
+
+    assert_eq!(f1, Some(&9));
+    assert_eq!(f2, Some(&8));
+    assert!(f3 > Some(&8));
 }
