1b250c965a841af3e1edc9b2ec9079086a287431
Takayuki Sato <s.yukkiy@gmail.com>
2022-01-19 00:47:05 +0900

    feat: Add test in parallel if any or all elements of a collection match a given predicate

--- rust/concurrency/parallel_tasks/src/main.rs
+++ rust/concurrency/parallel_tasks/src/main.rs
@@ -2,10 +2,29 @@ use rayon::prelude::*;
 
 fn main() {
     mutate_the_elements_of_an_array_in_parallel();
+    test_in_parallel_if_any_or_all_elements_of_a_collection_match_a_given_predicate();
 }
 
 fn mutate_the_elements_of_an_array_in_parallel() {
     let mut arr = [0, 7, 9, 11];
     arr.par_iter_mut().for_each(|p| *p -= 1);
     println!("{:?}", arr);
+}
+
+fn test_in_parallel_if_any_or_all_elements_of_a_collection_match_a_given_predicate() {
+    let mut vec = vec![2, 4, 6, 8];
+
+    assert!(!vec.par_iter().any(|n| (*n % 2) != 0));
+    assert!(vec.par_iter().all(|n| (*n % 2) == 0));
+    assert!(!vec.par_iter().any(|n| *n > 8));
+    assert!(vec.par_iter().all(|n| *n <= 8));
+
+    vec.push(9);
+
+    assert!(vec.par_iter().any(|n| (*n % 2) != 0));
+    assert!(!vec.par_iter().all(|n| (*n % 2) == 0));
+    assert!(vec.par_iter().any(|n| *n > 8));
+    assert!(!vec.par_iter().all(|n| *n <= 8));
+
+    println!("{:?}", vec);
 }
