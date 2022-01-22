be246e914db91059f1bce8ba94b46302581d5c40
Takayuki Sato <s.yukkiy@gmail.com>
2022-01-22 15:01:56 +0900

    feat: Add insert and query data

--- rust/database/postgres/src/main.rs
+++ rust/database/postgres/src/main.rs
@@ -1,5 +1,10 @@
 mod create_table;
+mod insert_data;
 
 fn main() {
     let _ = create_table::create_tables_in_a_postgres_database();
+    match insert_data::insert_and_query_data() {
+        Err(e) => println!("{}", e),
+        _ => (),
+    }
 }
