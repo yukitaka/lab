ad8af44faeca09b4bef4f9fb09eddcf59d0b6426
Takayuki Sato <s.yukkiy@gmail.com>
2022-01-25 00:32:01 +0900

    feat: Add log to stdout instead of stderr

--- rust/date_and_time/log_message/src/main.rs
+++ rust/date_and_time/log_message/src/main.rs
@@ -1,7 +1,12 @@
+use env_logger::{Builder, Target};
+use log;
+
 mod console;
 
 fn main() {
-    env_logger::init();
+    Builder::new().target(Target::Stdout).init();
+
+    log::error!("This error has been printed to Stdout");
 
     execute_query("DROP TABLE students");
     console::log_an_error_message_to_the_console();
