048c4daf16c52dd5625b57890c2553ba011c7247
Takayuki Sato <s.yukkiy@gmail.com>
2022-01-27 00:23:59 +0900

    feat: Add log messages to a custom location

---
+++ rust/development_tool/configure_logging/src/log4_location.rs
@@ -0,0 +1,19 @@
+use log::LevelFilter;
+use log4rs::append::file::FileAppender;
+use log4rs::config::{Appender, Config, Root};
+use log4rs::encode::pattern::PatternEncoder;
+
+pub fn log_messages_to_a_custom_location() {
+    let logfile = FileAppender::builder()
+        .encoder(Box::new(PatternEncoder::new("{l} - {m}\n")))
+        .build("log/output.log")
+        .unwrap();
+
+    let config = Config::builder()
+        .appender(Appender::builder().build("logfile", Box::new(logfile)))
+        .build(Root::builder().appender("logfile").build(LevelFilter::Info))
+        .unwrap();
+
+    let _ = log4rs::init_config(config);
+    log::info!("Hello, world!");
+}
--- rust/development_tool/configure_logging/src/main.rs
+++ rust/development_tool/configure_logging/src/main.rs
@@ -1,8 +1,10 @@
 mod custom_environment_logging;
+mod log4_location;
 mod per_module;
 mod timestamp_in_log;
 
 fn main() {
+    log4_location::log_messages_to_a_custom_location();
     timestamp_in_log::include_timestamp_in_log_messages();
     custom_environment_logging::use_a_custom_environment_variable_to_set_up_logging();
     per_module::enable_log_levels_per_module();
