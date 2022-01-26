c701734edfbfb531d4616db7ad5fa8af826029c0
Takayuki Sato <s.yukkiy@gmail.com>
2022-01-27 01:03:01 +0900

    feat: Add parse a complex version string

--- rust/development_tool/versioning/src/main.rs
+++ rust/development_tool/versioning/src/main.rs
@@ -4,6 +4,9 @@ fn main() {
     if let Err(e) = parse_and_increment_a_version_string() {
         print!("{}", e);
     }
+    if let Err(e) = parse_a_complex_version_string() {
+        println!("{}", e);
+    }
 }
 
 fn parse_and_increment_a_version_string() -> Result<(), Error> {
@@ -34,6 +37,28 @@ fn parse_and_increment_a_version_string() -> Result<(), Error> {
     parsed_version.patch = 0;
     assert_eq!(parsed_version.to_string(), "1.0.0");
     println!("New major release: v{}", parsed_version);
+
+    Ok(())
+}
+
+fn parse_a_complex_version_string() -> Result<(), Error> {
+    let version_str = "1.0.49-125+g72ee7853";
+    let parsed_version = Version::parse(version_str)?;
+
+    assert_eq!(
+        parsed_version,
+        Version {
+            major: 1,
+            minor: 0,
+            patch: 49,
+            pre: Prerelease::new("125")?,
+            build: BuildMetadata::new("g72ee7853")?,
+        }
+    );
+    assert_eq!(parsed_version.build, BuildMetadata::new("g72ee7853")?,);
+
+    let serialized_version = parsed_version.to_string();
+    assert_eq!(&serialized_version, version_str);
 
     Ok(())
 }
