5e82795e4dca5c5a52bf1820f6ee045969b639ba
Takayuki Sato <s.yukkiy@gmail.com>
2022-01-27 00:03:40 +0900

    feat: Add include timestamp in log messages

--- rust/development_tool/configure_logging/src/main.rs
+++ rust/development_tool/configure_logging/src/main.rs
@@ -1,7 +1,9 @@
 mod custom_environment_logging;
 mod per_module;
+mod timestamp_in_log;
 
 fn main() {
+    timestamp_in_log::include_timestamp_in_log_messages();
     custom_environment_logging::use_a_custom_environment_variable_to_set_up_logging();
     per_module::enable_log_levels_per_module();
 }
