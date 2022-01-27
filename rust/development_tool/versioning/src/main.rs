b98cce4c8a5d02828b8b0c983880630f4f2dc6d9
Takayuki Sato <s.yukkiy@gmail.com>
2022-01-28 00:23:17 +0900

    feat: Add find the latest version satisfying given range

--- rust/development_tool/versioning/src/main.rs
+++ rust/development_tool/versioning/src/main.rs
@@ -1,4 +1,4 @@
-use semver::{BuildMetadata, Error, Prerelease, Version};
+use semver::{BuildMetadata, Error, Prerelease, Version, VersionReq};
 
 fn main() {
     if let Err(e) = parse_and_increment_a_version_string() {
@@ -8,6 +8,9 @@ fn main() {
         println!("{}", e);
     }
     if let Err(e) = check_if_given_version_is_prerelease() {
+        println!("{}", e);
+    }
+    if let Err(e) = find_the_latest_version_satisfying_given_range() {
         println!("{}", e);
     }
 }
@@ -74,4 +77,43 @@ fn check_if_given_version_is_prerelease() -> Result<(), Error> {
     assert_eq!(version_2.pre, Prerelease::EMPTY);
 
     Ok(())
+}
+
+fn find_the_latest_version_satisfying_given_range() -> Result<(), Error> {
+    assert_eq!(
+        find_max_matching_version("<= 1.0.0", vec!["0.9.0", "1.0.0", "1.0.1"])?,
+        Some(Version::parse("1.0.0")?)
+    );
+
+    assert_eq!(
+        find_max_matching_version(
+            ">1.2.3-alpha.3",
+            vec![
+                "1.2.3-alpha.3",
+                "1.2.3-alpha.4",
+                "1.2.3-alpha.10",
+                "1.2.3-beta.4",
+                "3.4.5-alpha.9",
+            ]
+        )?,
+        Some(Version::parse("1.2.3-beta.4")?)
+    );
+
+    Ok(())
+}
+
+fn find_max_matching_version<'a, I>(
+    version_req_str: &str,
+    iterable: I,
+) -> Result<Option<Version>, Error>
+where
+    I: IntoIterator<Item = &'a str>,
+{
+    let vreq = VersionReq::parse(version_req_str)?;
+
+    Ok(iterable
+        .into_iter()
+        .filter_map(|s| Version::parse(s).ok())
+        .filter(|s| vreq.matches(s))
+        .max())
 }
