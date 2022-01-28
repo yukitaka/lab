725c16138ed8a71d52a1ff5f1ec135a3b85080e5
Takayuki Sato <s.yukkiy@gmail.com>
2022-01-29 01:15:41 +0900

    feat: Add encode a string as application/x-www-form-urlencoded

--- rust/encoding/character_sets/src/main.rs
+++ rust/encoding/character_sets/src/main.rs
@@ -1,7 +1,9 @@
 mod percent_encode;
+mod urlencoded;
 
 fn main() {
     if let Err(e) = percent_encode::percent_encode_a_string() {
         println!("{}", e);
     }
+    urlencoded::encode_a_string_as_application_s_www_form_urlencoded();
 }
---
+++ rust/encoding/character_sets/src/urlencoded.rs
@@ -0,0 +1,13 @@
+use url::form_urlencoded::{byte_serialize, parse};
+
+pub fn encode_a_string_as_application_s_www_form_urlencoded() {
+    let urlencoded: String = byte_serialize("What is ❤?".as_bytes()).collect();
+    assert_eq!(urlencoded, "What+is+%E2%9D%A4%3F");
+    println!("urlencoded:'{}'", urlencoded);
+
+    let decoded: String = parse(urlencoded.as_bytes())
+        .map(|(key, val)| [key, val].concat())
+        .collect();
+    assert_eq!(decoded, "What is ❤?");
+    println!("decoded:'{}'", decoded);
+}
