b88e23ecc2ce03ab0e7943394c1f9d30fe472fab
Takayuki Sato <s.yukkiy@gmail.com>
2022-01-30 00:56:48 +0900

    feat: Add read csv records deserialize

--- rust/encoding/csv_processing/src/main.rs
+++ rust/encoding/csv_processing/src/main.rs
@@ -1,7 +1,11 @@
 mod read_csv_records;
+mod read_deserialize;
 
 fn main() {
     if let Err(e) = read_csv_records::read_csv_records() {
+        println!("{}", e);
+    }
+    if let Err(e) = read_deserialize::read_csv_deserialize() {
         println!("{}", e);
     }
 }
