375b1bb80dfaf4736588457ad16b97fb602cfe1e
Takayuki Sato <s.yukkiy@gmail.com>
2022-02-02 00:40:36 +0900

    feat: Add avoid discarding errors during error conversions

--- rust/error_handling/src/main.rs
+++ rust/error_handling/src/main.rs
@@ -1,5 +1,7 @@
+mod avoid_discarding_errors;
 mod handle_error;
 
 fn main() {
     handle_error::handle_errors_correctly_in_main();
+    avoid_discarding_errors::avoid_discarding_errors_during_error_conversions();
 }
