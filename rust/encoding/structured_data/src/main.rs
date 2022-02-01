60bf004b55f7c31cd942b687cf4d66408fdaada1
Takayuki Sato <s.yukkiy@gmail.com>
2022-02-02 00:02:58 +0900

    feat: Add deserialize a toml using serde

--- rust/encoding/structured_data/src/main.rs
+++ rust/encoding/structured_data/src/main.rs
@@ -8,4 +8,7 @@ fn main() {
     if let Err(e) = toml_file::deserialize_a_toml_configuration_file() {
         println!("{}", e);
     }
+    if let Err(e) = toml_file::toml_using_serde() {
+        println!("{}", e);
+    }
 }
--- rust/encoding/structured_data/src/toml_file.rs
+++ rust/encoding/structured_data/src/toml_file.rs
@@ -1,3 +1,5 @@
+use serde::Deserialize;
+use std::collections::HashMap;
 use toml::{de::Error, Value};
 
 pub fn deserialize_a_toml_configuration_file() -> Result<(), Error> {
@@ -17,5 +19,38 @@ pub fn deserialize_a_toml_configuration_file() -> Result<(), Error> {
         package_info["package"]["name"].as_str(),
         Some("your_package")
     );
+    Ok(())
+}
+
+#[derive(Deserialize)]
+struct Config {
+    package: Package,
+    dependencies: HashMap<String, String>,
+}
+
+#[derive(Deserialize)]
+struct Package {
+    name: String,
+    version: String,
+    authors: Vec<String>,
+}
+
+pub fn toml_using_serde() -> Result<(), Error> {
+    let toml_content = r#"
+        [package]
+        name = "your_package"
+        version = "0.1.0"
+        authors = ["You! <you@example.org>"]
+
+        [dependencies]
+        serde = "1.0"
+        "#;
+    let package_info: Config = toml::from_str(toml_content)?;
+
+    assert_eq!(package_info.package.name, "your_package");
+    assert_eq!(package_info.package.version, "0.1.0");
+    assert_eq!(package_info.package.authors, vec!["You! <you@example.org>"]);
+    assert_eq!(package_info.dependencies["serde"], "1.0");
+
     Ok(())
 }
