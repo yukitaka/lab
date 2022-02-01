42182982986d355e53ddf087f6219827ec561abc
Takayuki Sato <s.yukkiy@gmail.com>
2022-02-02 00:10:22 +0900

    feat: Add read and write integers in little-endian byte order

--- rust/encoding/structured_data/src/main.rs
+++ rust/encoding/structured_data/src/main.rs
@@ -1,3 +1,4 @@
+mod little_endian_order;
 mod toml_file;
 mod unstructured_json;
 
@@ -9,6 +10,9 @@ fn main() {
         println!("{}", e);
     }
     if let Err(e) = toml_file::toml_using_serde() {
+        println!("{}", e);
+    }
+    if let Err(e) = little_endian_order::read_and_write_integers_in_little_endian_byte_order() {
         println!("{}", e);
     }
 }
