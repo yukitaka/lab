4c4a8e30d4a987b425f5f370c254e4946a9a9425
Takayuki Sato <s.yukkiy@gmail.com>
2022-02-01 10:04:57 +0900

    feat: Add deserialize a toml configuration file

--- rust/encoding/structured_data/src/main.rs
+++ rust/encoding/structured_data/src/main.rs
@@ -1,7 +1,11 @@
+mod toml_file;
 mod unstructured_json;
 
 fn main() {
     if let Err(e) = unstructured_json::serialize_and_deserialize_unstructured_json() {
+        println!("{}", e);
+    }
+    if let Err(e) = toml_file::deserialize_a_toml_configuration_file() {
         println!("{}", e);
     }
 }
---
+++ rust/encoding/structured_data/src/toml_file.rs
@@ -0,0 +1,21 @@
+use toml::{de::Error, Value};
+
+pub fn deserialize_a_toml_configuration_file() -> Result<(), Error> {
+    let toml_content = r#"
+        [package]
+        name = "your_package"
+        version = "0.1.0"
+        authors = ["You! <you@example.org>"]
+
+        [dependencies]
+        serde = "1.0"
+        "#;
+    let package_info: Value = toml::from_str(toml_content)?;
+
+    assert_eq!(package_info["dependencies"]["serde"].as_str(), Some("1.0"));
+    assert_eq!(
+        package_info["package"]["name"].as_str(),
+        Some("your_package")
+    );
+    Ok(())
+}
