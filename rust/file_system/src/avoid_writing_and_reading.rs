c7d118b33fbcfbfc6b36f1d0e380f26eb48f4812
Takayuki Sato <s.yukkiy@gmail.com>
2022-02-03 00:11:55 +0900

    feat: Add avoid writing and reading from a same file

---
+++ rust/file_system/src/avoid_writing_and_reading.rs
@@ -0,0 +1,25 @@
+use same_file::Handle;
+use std::fs::File;
+use std::io::{BufRead, BufReader, Error, ErrorKind};
+use std::path::Path;
+
+pub fn avoid_writing_and_reading_from_a_same_file() -> Result<(), Error> {
+    let path_to_read = Path::new("new.txt");
+    let stdout_handle = Handle::stdout()?;
+    let handle = Handle::from_path(path_to_read)?;
+
+    if stdout_handle == handle {
+        return Err(Error::new(
+            ErrorKind::Other,
+            "You are reading and writing to the same file",
+        ));
+    } else {
+        let file = File::open(&path_to_read)?;
+        let file = BufReader::new(file);
+        for (num, line) in file.lines().enumerate() {
+            println!("{} : {}", num, line?.to_uppercase());
+        }
+    }
+
+    Ok(())
+}
--- rust/file_system/src/main.rs
+++ rust/file_system/src/main.rs
@@ -1,7 +1,11 @@
+mod avoid_writing_and_reading;
 mod read_lines;
 
 fn main() {
     if let Err(e) = read_lines::read_lines_of_strings_from_a_file() {
+        println!("{}", e);
+    }
+    if let Err(e) = avoid_writing_and_reading::avoid_writing_and_reading_from_a_same_file() {
         println!("{}", e);
     }
 }
