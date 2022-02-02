6da107e0d41eb54e26e7181789c18e0b130f2302
Takayuki Sato <s.yukkiy@gmail.com>
2022-02-03 00:01:57 +0900

    feat: Add obtain backtrace of complex error scenarios

--- rust/error_handling/src/main.rs
+++ rust/error_handling/src/main.rs
@@ -1,7 +1,9 @@
 mod avoid_discarding_errors;
+mod error_scenarios;
 mod handle_error;
 
 fn main() {
     handle_error::handle_errors_correctly_in_main();
     avoid_discarding_errors::avoid_discarding_errors_during_error_conversions();
+    error_scenarios::obtain_backtrace_of_complex_error_scenarios();
 }
