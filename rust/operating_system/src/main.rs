a14ad035f42df3fd411d80631bc27176d2d225f7
Takayuki Sato <s.yukkiy@gmail.com>
2022-02-07 00:35:15 +0900

    feat: Add run an external command passing it stdin and check for an error code

--- rust/operating_system/src/main.rs
+++ rust/operating_system/src/main.rs
@@ -1,7 +1,13 @@
 mod process_stdout;
+mod stdin_and_check;
 
 fn main() {
     if let Err(e) = process_stdout::run_an_external_command_and_process_stdout() {
+        println!("{}", e);
+    }
+    if let Err(e) =
+        stdin_and_check::run_an_external_command_passing_it_stdin_and_check_for_an_error_code()
+    {
         println!("{}", e);
     }
 }
