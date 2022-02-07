f2196f606221dec1973786bb57776555f323f2bc
Takayuki Sato <s.yukkiy@gmail.com>
2022-02-08 01:02:22 +0900

    feat: Add multiply a scalar with a vector with a matrix

--- rust/science/mathematics/src/main.rs
+++ rust/science/mathematics/src/main.rs
@@ -4,4 +4,5 @@ mod multiplying_matrices;
 fn main() {
     adding_matrices::adding_matrices();
     multiplying_matrices::multiplying_matrices();
+    multiplying_matrices::multiply_a_scalar_with_a_vector_with_a_matrix();
 }
--- rust/science/mathematics/src/multiplying_matrices.rs
+++ rust/science/mathematics/src/multiplying_matrices.rs
@@ -1,4 +1,4 @@
-use ndarray::arr2;
+use ndarray::{arr1, arr2, Array1};
 
 pub fn multiplying_matrices() {
     let a = arr2(&[[1, 2, 3], [4, 5, 6]]);
@@ -6,4 +6,17 @@ pub fn multiplying_matrices() {
 
     println!("Multiplying matrices");
     println!("{}", a.dot(&b));
+}
+
+pub fn multiply_a_scalar_with_a_vector_with_a_matrix() {
+    let scalar = 4;
+    let vector = arr1(&[1, 2, 3]);
+    let matrix = arr2(&[[4, 5, 6], [7, 8, 9]]);
+
+    println!("Multiply a scalar with a vector with a matrix");
+    let new_vector: Array1<_> = scalar * vector;
+    println!("{}", new_vector);
+
+    let new_matrix = matrix.dot(&new_vector);
+    println!("{}", new_matrix);
 }
