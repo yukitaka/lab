6467afcd77be38554b1b9561a0911c292ad4be25
Takayuki Sato <s.yukkiy@gmail.com>
2022-02-09 00:57:18 +0900

    feat: Add vector norm

--- rust/science/mathematics/src/main.rs
+++ rust/science/mathematics/src/main.rs
@@ -1,10 +1,12 @@
 mod adding_matrices;
 mod multiplying_matrices;
 mod vector_comparison;
+mod vector_norm;
 
 fn main() {
     adding_matrices::adding_matrices();
     multiplying_matrices::multiplying_matrices();
     multiplying_matrices::multiply_a_scalar_with_a_vector_with_a_matrix();
     vector_comparison::vector_comparison();
+    vector_norm::vector_norm();
 }
