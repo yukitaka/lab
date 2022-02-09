e954bb90e65b0e4a60cbd7bde3ae9d4d1b1641a9
Takayuki Sato <s.yukkiy@gmail.com>
2022-02-10 00:51:53 +0900

    feat: Add distance between two points on the earth

--- rust/science/mathematics/trigonometry/src/main.rs
+++ rust/science/mathematics/trigonometry/src/main.rs
@@ -1,6 +1,7 @@
 fn main() {
     calculating_the_side_length_of_a_triangle();
     verifying_tan_is_equal_to_sin_divided_by_cos();
+    distance_between_two_points_on_the_earth();
 }
 
 fn calculating_the_side_length_of_a_triangle() {
@@ -17,4 +18,27 @@ fn verifying_tan_is_equal_to_sin_divided_by_cos() {
     let b = x.sin() / x.cos();
 
     assert_eq!(a, b);
+}
+
+fn distance_between_two_points_on_the_earth() {
+    let earth_radius_kilometer = 6371.0_f64;
+    let (paris_latitude_degrees, paris_longitude_degrees) = (48.85341_f64, -2.34800_f64);
+    let (london_latitude_degrees, london_longitude_degrees) = (51.50853_f64, -0.12574_f64);
+
+    let paris_latitude = paris_latitude_degrees.to_radians();
+    let london_latitude = london_latitude_degrees.to_radians();
+
+    let delta_latitude = (paris_latitude_degrees - london_latitude_degrees).to_radians();
+    let delta_longitude = (paris_longitude_degrees - london_longitude_degrees).to_radians();
+
+    let central_angle_inner = (delta_latitude / 2.0).sin().powi(2)
+        + paris_latitude.cos() * london_latitude.cos() * (delta_longitude / 2.0).sin().powi(2);
+    let central_angle = 2.0 * central_angle_inner.sqrt().asin();
+
+    let distance = earth_radius_kilometer * central_angle;
+
+    println!(
+        "Distance between Paris and London on the surface of Earth is {:.1} kilometers",
+        distance
+    );
 }
