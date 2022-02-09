9c893168f0ce7a83a02031d7bd5544d2fd27f913
Takayuki Sato <s.yukkiy@gmail.com>
2022-02-10 00:45:00 +0900

    feat: Add verifying tan is equal to sin divided by cos

--- rust/science/mathematics/trigonometry/src/main.rs
+++ rust/science/mathematics/trigonometry/src/main.rs
@@ -1,5 +1,6 @@
 fn main() {
     calculating_the_side_length_of_a_triangle();
+    verifying_tan_is_equal_to_sin_divided_by_cos();
 }
 
 fn calculating_the_side_length_of_a_triangle() {
@@ -8,4 +9,12 @@ fn calculating_the_side_length_of_a_triangle() {
     let hypotenuse = side_length / angle.sin();
 
     println!("Hypotenuse: {}", hypotenuse);
+}
+
+fn verifying_tan_is_equal_to_sin_divided_by_cos() {
+    let x: f64 = 6.0;
+    let a = x.tan();
+    let b = x.sin() / x.cos();
+
+    assert_eq!(a, b);
 }
