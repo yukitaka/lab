82cbd069c3120227eb419d913fc4f8199d48d031
Takayuki Sato <s.yukkiy@gmail.com>
2022-02-11 00:43:26 +0900

    feat: Add creating complex numbers

---
+++ rust/science/mathematics/complex_numbers/src/main.rs
@@ -0,0 +1,11 @@
+fn main() {
+    creating_complex_numbers();
+}
+
+fn creating_complex_numbers() {
+    let complex_integer = num::complex::Complex::new(10, 20);
+    let complex_float = num::complex::Complex::new(10.1, 20.1);
+
+    println!("Complex integer: {}", complex_integer);
+    println!("Complex float: {}", complex_float);
+}
