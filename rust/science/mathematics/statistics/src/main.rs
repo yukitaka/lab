8d730c62b0af1a6e3936dedd6aa1b4494a672b35
Takayuki Sato <s.yukkiy@gmail.com>
2022-02-12 00:58:51 +0900

    feat: Add standard deviation

--- rust/science/mathematics/statistics/src/main.rs
+++ rust/science/mathematics/statistics/src/main.rs
@@ -1,8 +1,10 @@
 mod measures_of_central;
 mod quick_select;
+mod standard_deviation;
 
 fn main() {
     measures_of_central::measures_of_central_tendency();
     quick_select::quick_select();
     measures_of_central::using_hashmap();
+    standard_deviation::standard_deviation();
 }
