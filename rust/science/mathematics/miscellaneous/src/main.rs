87666aa796652fe737195b8ffff2571d3abf139d
Takayuki Sato <s.yukkiy@gmail.com>
2022-02-13 00:10:11 +0900

    feat: Add big integers

---
+++ rust/science/mathematics/miscellaneous/src/main.rs
@@ -0,0 +1,16 @@
+use num::bigint::{BigInt, ToBigInt};
+
+fn main() {
+    println!("{}! equals {}", 100, factorial(100));
+}
+
+fn factorial(x: i32) -> BigInt {
+    if let Some(mut factorial) = 1.to_bigint() {
+        for i in 1..=x {
+            factorial = factorial * i;
+        }
+        factorial
+    } else {
+        panic!("Failed to calculate factorial!");
+    }
+}
