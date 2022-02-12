072088d81fbacae0801e4b1555ef8ea2572b0a48
Takayuki Sato <s.yukkiy@gmail.com>
2022-02-13 00:47:04 +0900

    feat: Add extract phone numbers from text

--- rust/text_processing/regular_expressions/src/main.rs
+++ rust/text_processing/regular_expressions/src/main.rs
@@ -1,7 +1,11 @@
 mod hashtags;
 mod login;
+mod phone;
 
 fn main() {
     login::verify_and_extract_login_from_an_email_address();
     hashtags::extract_a_list_of_unique_hashtags_from_a_text();
+    if let Err(e) = phone::extract_phone_numbers_from_text() {
+        println!("{}", e);
+    }
 }
