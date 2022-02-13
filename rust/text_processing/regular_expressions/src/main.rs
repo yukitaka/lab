8654f4b06bae5cf0318e139dc281879a09af77cc
Takayuki Sato <s.yukkiy@gmail.com>
2022-02-14 00:25:06 +0900

    feat: Add filter a log file by matching multiple regular expressions

--- rust/text_processing/regular_expressions/src/main.rs
+++ rust/text_processing/regular_expressions/src/main.rs
@@ -1,11 +1,17 @@
 mod hashtags;
 mod login;
+mod multiple_regular_expressions;
 mod phone;
 
 fn main() {
     login::verify_and_extract_login_from_an_email_address();
     hashtags::extract_a_list_of_unique_hashtags_from_a_text();
     if let Err(e) = phone::extract_phone_numbers_from_text() {
+        println!("{}", e);
+    }
+    if let Err(e) =
+        multiple_regular_expressions::filter_a_log_file_by_matching_multiple_regular_expressions()
+    {
         println!("{}", e);
     }
 }
