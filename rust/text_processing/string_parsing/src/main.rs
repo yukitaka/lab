8332c275bae388d747f71ade6ab35d855ab24af0
Takayuki Sato <s.yukkiy@gmail.com>
2022-02-14 00:50:06 +0900

    feat: Add implement the FromStr trait for a custom struct

--- rust/text_processing/string_parsing/src/main.rs
+++ rust/text_processing/string_parsing/src/main.rs
@@ -1,5 +1,6 @@
 fn main() {
     collect_unicode_graphemes();
+    implement_the_from_str_trait_for_a_custom_struct();
 }
 
 fn collect_unicode_graphemes() {
@@ -8,4 +9,47 @@ fn collect_unicode_graphemes() {
     let name = "José Guimarães\r\n";
     let graphemes = UnicodeSegmentation::graphemes(name, true).collect::<Vec<&str>>();
     assert_eq!(graphemes[3], "é");
+}
+
+use std::str::FromStr;
+
+#[derive(Debug, PartialEq)]
+struct RGB {
+    r: u8,
+    g: u8,
+    b: u8,
+}
+
+impl FromStr for RGB {
+    type Err = std::num::ParseIntError;
+
+    fn from_str(hex_code: &str) -> Result<Self, Self::Err> {
+        let r: u8 = u8::from_str_radix(&hex_code[1..3], 16)?;
+        let g: u8 = u8::from_str_radix(&hex_code[3..5], 16)?;
+        let b: u8 = u8::from_str_radix(&hex_code[5..7], 16)?;
+        Ok(RGB { r, g, b })
+    }
+}
+
+fn implement_the_from_str_trait_for_a_custom_struct() {
+    let code: &str = &r"#fa7268";
+    match RGB::from_str(code) {
+        Ok(rgb) => {
+            println!(
+                r"The RGB color code is: R: {} G: {} B: {}",
+                rgb.r, rgb.g, rgb.b
+            );
+        }
+        Err(_) => {
+            println!("{} is not a valid color hex code!", code);
+        }
+    }
+    assert_eq!(
+        RGB::from_str(&r"#fa7268").unwrap(),
+        RGB {
+            r: 250,
+            g: 114,
+            b: 104,
+        }
+    );
 }
