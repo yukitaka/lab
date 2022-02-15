e26b20e2e657b0bc73d430734ae77f77dbdedbf3
Takayuki Sato <s.yukkiy@gmail.com>
2022-02-16 00:00:28 +0900

    fix: typo

--- rust/web/url/src/main.rs
+++ rust/web/url/src/main.rs
@@ -1,7 +1,7 @@
 mod parse_url;
 
 fn main() {
-    if let Err(e) = parse_url::parse_a_url_from_a_string_to_a_rul_type() {
+    if let Err(e) = parse_url::parse_a_url_from_a_string_to_a_url_type() {
         println!("{}", e);
     }
 }
--- rust/web/url/src/parse_url.rs
+++ rust/web/url/src/parse_url.rs
@@ -1,6 +1,6 @@
 use url::{ParseError, Url};
 
-pub fn parse_a_url_from_a_string_to_a_rul_type() -> Result<(), ParseError> {
+pub fn parse_a_url_from_a_string_to_a_url_type() -> Result<(), ParseError> {
     let s = "https://github.com/rust-lang/rust/issues?labels=E-easy&state=open";
     let parsed = Url::parse(s)?;
     println!("The path part of the URL is: {}", parsed.path());
