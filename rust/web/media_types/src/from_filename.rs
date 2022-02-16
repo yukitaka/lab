10e97e978885b6c02fc64897d6c39f5e4573397d
Takayuki Sato <s.yukkiy@gmail.com>
2022-02-17 00:22:10 +0900

    feat: Add get mime type from filename

---
+++ rust/web/media_types/src/from_filename.rs
@@ -0,0 +1,23 @@
+use mime::Mime;
+
+pub fn get_mime_type_from_filename() {
+    let filenames = vec!["foobar.jpg", "foo.bar", "foobar.png"];
+    for file in filenames {
+        let mime = find_mimetype(&file.to_owned());
+        println!("MIME for {}: {}", file, mime);
+    }
+}
+
+fn find_mimetype(filename: &String) -> Mime {
+    let parts: Vec<&str> = filename.split('.').collect();
+    let res = match parts.last() {
+        Some(v) => match *v {
+            "png" => mime::IMAGE_PNG,
+            "jpg" => mime::IMAGE_JPEG,
+            "json" => mime::APPLICATION_JSON,
+            &_ => mime::TEXT_PLAIN,
+        },
+        None => mime::TEXT_PLAIN,
+    };
+    return res;
+}
--- rust/web/media_types/src/main.rs
+++ rust/web/media_types/src/main.rs
@@ -1,5 +1,7 @@
+mod from_filename;
 mod from_string;
 
 fn main() {
     from_string::get_mime_type_from_string();
+    from_filename::get_mime_type_from_filename();
 }
