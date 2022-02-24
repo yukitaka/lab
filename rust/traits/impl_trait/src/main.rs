0615defb3b4defcae7e3146d1b5c19997afae58f
Takayuki Sato <s.yukkiy@gmail.com>
2022-02-25 00:15:28 +0900

    feat: add a return type impl

--- rust/traits/impl_trait/src/main.rs
+++ rust/traits/impl_trait/src/main.rs
@@ -1,7 +1,9 @@
 mod argument_type;
+mod return_type;
 
 fn main() {
     if let Err(e) = argument_type::return_type() {
         println!("{}", e);
     }
+    return_type::return_type();
 }
---
+++ rust/traits/impl_trait/src/return_type.rs
@@ -0,0 +1,26 @@
+pub fn return_type() {
+    let v1 = vec![1, 2, 3];
+    let v2 = vec![4, 5];
+    let mut v3 = combine_vecs(v1, v2);
+    assert_eq!(Some(1), v3.next());
+    assert_eq!(Some(2), v3.next());
+    assert_eq!(Some(3), v3.next());
+    assert_eq!(Some(4), v3.next());
+    assert_eq!(Some(5), v3.next());
+    println!("all done");
+}
+
+/*
+use std::iter;
+use std::vec::IntoIter;
+fn combine_vecs_explicit_return_type(
+    v: Vec<i32>,
+    u: Vec<i32>,
+) -> iter::Cycle<iter::Chain<IntoIter<i32>, IntoIter<i32>>> {
+    v.into_iter().chain(u.into_iter()).cycle()
+}
+*/
+
+fn combine_vecs(v: Vec<i32>, u: Vec<i32>) -> impl Iterator<Item = i32> {
+    v.into_iter().chain(u.into_iter()).cycle()
+}
