949875db3f8a1b6f0b6de24f28a81c8f6cc71805
Takayuki Sato <s.yukkiy@gmail.com>
2022-03-01 01:11:08 +0900

    feat: add unpacking options with ?

---
+++ rust/error_handling/unpacking_options/src/main.rs
@@ -0,0 +1,34 @@
+struct Person {
+    job: Option<Job>,
+}
+
+#[derive(Clone, Copy)]
+struct Job {
+    phone_number: Option<PhoneNumber>,
+}
+
+#[derive(Clone, Copy)]
+#[allow(dead_code)]
+struct PhoneNumber {
+    area_code: Option<u8>,
+    number: u32,
+}
+
+impl Person {
+    fn work_phone_area_code(&self) -> Option<u8> {
+        self.job?.phone_number?.area_code
+    }
+}
+
+fn main() {
+    let p = Person {
+        job: Some(Job {
+            phone_number: Some(PhoneNumber {
+                area_code: Some(61),
+                number: 439222222,
+            }),
+        }),
+    };
+
+    assert_eq!(p.work_phone_area_code(), Some(61));
+}
