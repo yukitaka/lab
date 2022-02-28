bb720ca277317fc1558efc01c4326f54deae0060
Takayuki Sato <s.yukkiy@gmail.com>
2022-03-01 00:01:31 +0900

    feat: add option & unwrap basic

---
+++ rust/error_handling/option_and_unwrap/src/main.rs
@@ -0,0 +1,33 @@
+fn give_adult(drink: Option<&str>) {
+    match drink {
+        Some("lemonade") => println!("Yuck! Too sugary."),
+        Some(inner) => println!("{}? How nice.", inner),
+        None => println!("No drink? Oh well."),
+    }
+}
+
+fn drink(drink: Option<&str>) {
+    let inside = drink.unwrap();
+    if inside == "lemonade" {
+        panic!("AAAaaaaa!!!!");
+    }
+
+    println!("I love {}s!!!!!", inside);
+}
+
+fn main() {
+    let water = Some("water");
+    let lemonade = Some("lemonade");
+    let void = None;
+
+    give_adult(water);
+    give_adult(lemonade);
+    give_adult(void);
+
+    let coffee = Some("coffee");
+    drink(coffee);
+    /*
+    let nothing = None;
+    drink(nothing);
+     */
+}
