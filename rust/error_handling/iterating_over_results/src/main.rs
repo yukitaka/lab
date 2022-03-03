41c7ce1fc3603775babba24e82b4b012e0bb294b
Takayuki Sato <s.yukkiy@gmail.com>
2022-03-04 00:58:05 +0900

    feat: add iterating over results

---
+++ rust/error_handling/iterating_over_results/src/main.rs
@@ -0,0 +1,50 @@
+fn main() {
+    iter_map();
+    filter_map();
+    collect();
+    partition();
+    unwrapped_partition();
+}
+
+fn iter_map() {
+    let strings = vec!["tofu", "93", "18"];
+    let numbers: Vec<_> = strings.into_iter().map(|s| s.parse::<i32>()).collect();
+    println!("Results: {:?}", numbers);
+}
+
+fn filter_map() {
+    let strings = vec!["tofu", "93", "18"];
+    let numbers: Vec<_> = strings
+        .into_iter()
+        .filter_map(|s| s.parse::<i32>().ok())
+        .collect();
+    println!("Results: {:?}", numbers);
+}
+
+fn collect() {
+    let strings = vec!["tofu", "93", "18"];
+    let numbers: Result<Vec<_>, _> = strings.into_iter().map(|s| s.parse::<i32>()).collect();
+    println!("Results: {:?}", numbers);
+}
+
+fn partition() {
+    let strings = vec!["tofu", "93", "18"];
+    let (numbers, errors): (Vec<_>, Vec<_>) = strings
+        .into_iter()
+        .map(|s| s.parse::<i32>())
+        .partition(Result::is_ok);
+    println!("Numbers: {:?}", numbers);
+    println!("Errors: {:?}", errors);
+}
+
+fn unwrapped_partition() {
+    let strings = vec!["tofu", "93", "18"];
+    let (numbers, errors): (Vec<_>, Vec<_>) = strings
+        .into_iter()
+        .map(|s| s.parse::<i32>())
+        .partition(Result::is_ok);
+    let numbers: Vec<_> = numbers.into_iter().map(Result::unwrap).collect();
+    let errors: Vec<_> = errors.into_iter().map(Result::unwrap_err).collect();
+    println!("Numbers: {:?}", numbers);
+    println!("Errors: {:?}", errors);
+}
