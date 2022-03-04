f92dbbf1f87b6d98c02a56daff6ee7313acf5115
Takayuki Sato <s.yukkiy@gmail.com>
2022-03-05 00:26:31 +0900

    feat: add strings literals and escapes with backslash

--- rust/std_library_types/strings/src/main.rs
+++ rust/std_library_types/strings/src/main.rs
@@ -1,5 +1,6 @@
 fn main() {
     basic();
+    literals();
 }
 
 fn basic() {
@@ -32,4 +33,23 @@ fn basic() {
 
     println!("Alice says: {}", alice);
     println!("Bob says: {}", bob);
+}
+
+fn literals() {
+    let byte_escape = "I'm writing \x52\x75\x73\x74!";
+    println!("What are you doing\x3F (\\x3F means ?) {}", byte_escape);
+
+    let unicode_codepoint = "\u{211D}";
+    let character_name = "\"DOUBLE-STRUCK CAPITAL R\"";
+
+    println!(
+        "Unicode character {} (U+211D) is called {}",
+        unicode_codepoint, character_name
+    );
+
+    let long_string = "String literals
+                              can span multiple lines.
+                              The linebreak and indentation here ->\
+                              <- can be escaped too!";
+    println!("{}", long_string);
 }
