58a7da90e068a4a4fab04271fa509b92e279282a
Takayuki Sato <s.yukkiy@gmail.com>
2022-03-06 00:32:13 +0900

    feat: add using panic and check memory leak

---
+++ rust/std_library_types/panic/src/main.rs
@@ -0,0 +1,14 @@
+// Check memory leak `valgrind ./target/debug/panic`
+fn division(dividend: i32, divisor: i32) -> i32 {
+    if divisor == 0 {
+        panic!("division by zero");
+    } else {
+        dividend / divisor
+    }
+}
+
+fn main() {
+    let _x = Box::new(0i32);
+    division(3, 0);
+    println!("This point won't be reached!");
+}
