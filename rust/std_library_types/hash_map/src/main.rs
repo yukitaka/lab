4574164a9d594a7dd81ad8e49f0c8a4a65d16995
Takayuki Sato <s.yukkiy@gmail.com>
2022-03-07 01:37:17 +0900

    feat: implements logon using hashmap

--- rust/std_library_types/hash_map/src/main.rs
+++ rust/std_library_types/hash_map/src/main.rs
@@ -1,5 +1,7 @@
 use std::collections::HashMap;
 
+mod logon;
+
 fn call(number: &str) -> &str {
     match number {
         "798-1364" => {
@@ -39,4 +41,6 @@ fn main() {
     for (contact, &number) in contacts.iter() {
         println!("Calling {}: {}", contact, call(number));
     }
+
+    logon::account_logon();
 }
