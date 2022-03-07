e0e15f443aabe9d5a2ed7eab123005a05f2d17fa
Takayuki Sato <s.yukkiy@gmail.com>
2022-03-08 00:34:20 +0900

    feat: add channels

---
+++ rust/threads/channels/src/main.rs
@@ -0,0 +1,30 @@
+use std::sync::mpsc;
+use std::sync::mpsc::{Receiver, Sender};
+use std::thread;
+
+static NTHREADS: i32 = 3;
+
+fn main() {
+    let (tx, rx): (Sender<i32>, Receiver<i32>) = mpsc::channel();
+    let mut children = Vec::new();
+
+    for id in 0..NTHREADS {
+        let thread_tx = tx.clone();
+
+        let child = thread::spawn(move || {
+            thread_tx.send(id).unwrap();
+            println!("thread {} finished", id);
+        });
+        children.push(child);
+    }
+    let mut ids = Vec::with_capacity(NTHREADS as usize);
+    for _ in 0..NTHREADS {
+        ids.push(rx.recv());
+    }
+
+    for child in children {
+        child.join().expect("oops! the child thread panicked");
+    }
+
+    println!("{:?}", ids);
+}
