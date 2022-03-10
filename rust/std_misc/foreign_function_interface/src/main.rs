e932937735ed2ca1af82edbccc026b0618bb0ff7
Takayuki Sato <s.yukkiy@gmail.com>
2022-03-11 00:33:10 +0900

    feat: implement using foreign function interface

---
+++ rust/std_misc/foreign_function_interface/src/main.rs
@@ -0,0 +1,35 @@
+use std::fmt;
+
+#[link(name = "m")]
+extern "C" {
+    fn csqrtf(z: Complex) -> Complex;
+    fn ccosf(z: Complex) -> Complex;
+}
+
+fn cos(z: Complex) -> Complex {
+    unsafe { ccosf(z) }
+}
+
+#[repr(C)]
+#[derive(Clone, Copy)]
+struct Complex {
+    re: f32,
+    im: f32,
+}
+
+impl fmt::Debug for Complex {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        if self.im < 0. {
+            write!(f, "{}-{}i", self.re, -self.im)
+        } else {
+            write!(f, "{}+{}i", self.re, self.im)
+        }
+    }
+}
+
+fn main() {
+    let z = Complex { re: -1., im: 0. };
+    let z_sqrt = unsafe { csqrtf(z) };
+    println!("the square root of {:?} is {:?}", z, z_sqrt);
+    println!("cos({:?}) = {:?}", z, cos(z));
+}
