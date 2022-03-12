75f00597d6020e07d41d66106217ea748246e6a3
Takayuki Sato <s.yukkiy@gmail.com>
2022-03-13 00:35:34 +0900

    feat: add documentation testing with error

--- rust/testing/documentation_testing/src/lib.rs
+++ rust/testing/documentation_testing/src/lib.rs
@@ -36,3 +36,24 @@ pub fn div(a: i32, b: i32) -> i32 {
     }
     a / b
 }
+
+/// Using hidden `try_main` in doc tests.
+///
+/// ```
+/// # // hidden lines start with `#` symbol, but they're still compilable!
+/// # fn try_main() -> Result<(), String> { // line that wraps the body shown in doc}
+/// let res = documentation_testing::try_div(10, 2)?;
+/// #   Ok(()) // returning from try_main
+/// # }
+/// # fn main() { // starting main that'll unwrap()
+/// #    try_main().unwrap(); // calling try_main and unwrapping
+/// #                         // so that test will panic in case of error
+/// # }
+/// ```
+pub fn try_div(a: i32, b: i32) -> Result<i32, String> {
+    if b == 0 {
+        Err(String::from("Divide-by-zero"))
+    } else {
+        Ok(a / b)
+    }
+}
