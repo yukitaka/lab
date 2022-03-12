0149223b0e9c1084f1803997120677619d048e2f
Takayuki Sato <s.yukkiy@gmail.com>
2022-03-12 20:03:45 +0900

    feat: add unit testing using early error

--- rust/testing/unit_testing/src/main.rs
+++ rust/testing/unit_testing/src/main.rs
@@ -11,6 +11,15 @@ fn bad_add(a: i32, b: i32) -> i32 {
     a - b
 }
 
+#[allow(dead_code)]
+fn sqrt(number: f64) -> Result<f64, String> {
+    if number >= 0.0 {
+        Ok(number.powf(0.5))
+    } else {
+        Err("negative floats don't have square roots".to_owned())
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -23,5 +32,12 @@ mod tests {
     #[test]
     fn test_bad_add() {
         assert_ne!(bad_add(1, 2), 3);
+    }
+
+    #[test]
+    fn test_sqrt() -> Result<(), String> {
+        let x = 4.0;
+        assert_eq!(sqrt(x)?.powf(2.0), x);
+        Ok(())
     }
 }
