b7c9bb2b60656f0c120877fe566db370ac87c518
Takayuki Sato <s.yukkiy@gmail.com>
2022-03-14 00:19:01 +0900

    feat: inline assembly imputs and outputs

--- rust/unsafe_operations/inline_assembly/src/main.rs
+++ rust/unsafe_operations/inline_assembly/src/main.rs
@@ -1,7 +1,10 @@
 use std::arch::asm;
 
 fn main() {
+    let x: u64 = 3;
+    let y: u64;
     unsafe {
-        asm!("nop");
+        asm!("add {0}, 5", inout(reg) x => y);
     }
+    assert_eq!(y, 8);
 }
