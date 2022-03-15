a63e9b0b65f3d81e2056e7e3e72e4bca8768b5da
Takayuki Sato <s.yukkiy@gmail.com>
2022-03-16 00:36:04 +0900

    feat: labels

--- rust/unsafe_operations/template_modifiers/src/main.rs
+++ rust/unsafe_operations/template_modifiers/src/main.rs
@@ -3,6 +3,7 @@ use std::arch::asm;
 fn main() {
     register_template_modifiers();
     load_fpu_control_word(10);
+    labels();
 }
 
 fn register_template_modifiers() {
@@ -19,4 +20,23 @@ fn load_fpu_control_word(control: u16) {
     unsafe {
         asm!("fldcw [{}]", in(reg) &control, options(nostack));
     }
+}
+
+fn labels() {
+    #[allow(unused_assignments)]
+    let mut a = 0;
+    unsafe {
+        asm!(
+            "mov {0:e}, 10",
+            "2:",
+            "sub {0:e}, 1",
+            "cmp {0:e}, 3",
+            "jle 2f",
+            "jmp 2b",
+            "2:",
+            "add {0:e}, 2",
+            out(reg) a,
+        );
+    }
+    assert_eq!(a, 5);
 }
