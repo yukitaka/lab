6ecaca4565339448f4667439f8d781b9b33f28ff
Takayuki Sato <s.yukkiy@gmail.com>
2022-03-21 12:45:55 +0900

    feat: implement write!

--- rust/operating_system/blog_os/src/vga_buffer.rs
+++ rust/operating_system/blog_os/src/vga_buffer.rs
@@ -1,3 +1,4 @@
+use core::fmt;
 use core::ops::{Deref, DerefMut};
 use volatile::Volatile;
 
@@ -24,6 +25,7 @@ pub enum Color {
 }
 
 pub fn print_something() {
+    use core::fmt::Write;
     let mut writer = Writer {
         column_position: 0,
         color_code: ColorCode::new(Color::Yellow, Color::Black),
@@ -32,7 +34,7 @@ pub fn print_something() {
 
     writer.write_byte(b'H');
     writer.write_string("ello ");
-    writer.write_string("Wörld!");
+    write!(writer, "The numbers are {} and {}", 42, 1.0 / 3.0).unwrap();
 }
 
 pub struct Writer {
@@ -73,6 +75,13 @@ impl Writer {
     }
 
     fn new_line(&mut self) {}
+}
+
+impl fmt::Write for Writer {
+    fn write_str(&mut self, s: &str) -> fmt::Result {
+        self.write_string(s);
+        Ok(())
+    }
 }
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
