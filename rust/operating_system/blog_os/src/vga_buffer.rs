c90d1d6d55044de6e9c5471e80b92eb33e0f4f06
Takayuki Sato <s.yukkiy@gmail.com>
2022-03-22 01:32:21 +0900

    feat: implement print! and println!

--- rust/operating_system/blog_os/src/vga_buffer.rs
+++ rust/operating_system/blog_os/src/vga_buffer.rs
@@ -4,6 +4,23 @@ use lazy_static::lazy_static;
 use spin::Mutex;
 use volatile::Volatile;
 
+#[macro_export]
+macro_rules! print {
+    ($($arg:tt)*) => ($crate::vga_buffer::_print(format_args!($($arg)*)));
+}
+
+#[macro_export]
+macro_rules! println {
+    () => ($crate::print!("\n"));
+    ($($arg:tt)*) => ($crate::print!("{}\n", format_args!($($arg)*)));
+}
+
+#[doc(hidden)]
+pub fn _print(args: fmt::Arguments) {
+    use core::fmt::Write;
+    WRITER.lock().write_fmt(args).unwrap();
+}
+
 lazy_static! {
     pub static ref WRITER: Mutex<Writer> = Mutex::new(Writer {
         column_position: 0,
