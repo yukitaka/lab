8e9c2e6840d3c1342c55434e27c1487677651041
Takayuki Sato <s.yukkiy@gmail.com>
2022-03-23 00:26:43 +0900

    feat: assertion example

--- rust/operating_system/blog_os/src/main.rs
+++ rust/operating_system/blog_os/src/main.rs
@@ -1,6 +1,6 @@
 #![feature(custom_test_frameworks)]
 #![test_runner(crate::test_runner)]
-
+#![reexport_test_harness_main = "test_main"]
 #![no_std]
 #![no_main]
 
@@ -11,6 +11,9 @@ mod vga_buffer;
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
     println!("Hello World{}", "!");
+
+    #[cfg(test)]
+    test_main();
 
     loop {}
 }
@@ -27,4 +30,11 @@ fn test_runner(tests: &[&dyn Fn()]) {
     for test in tests {
         test();
     }
+}
+
+#[test_case]
+fn trivial_assertion() {
+    print!("trivial assertion...");
+    assert_eq!(1, 1);
+    println!("[ok]");
 }
