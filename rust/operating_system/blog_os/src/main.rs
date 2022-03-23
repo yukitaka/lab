c545ad5173a2b5a3873d293250fd80009766a649
Takayuki Sato <s.yukkiy@gmail.com>
2022-03-24 00:55:24 +0900

    feat: redirect panic test message via serial

--- rust/operating_system/blog_os/src/main.rs
+++ rust/operating_system/blog_os/src/main.rs
@@ -19,9 +19,19 @@ pub extern "C" fn _start() -> ! {
     loop {}
 }
 
+#[cfg(not(test))]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     println!("{}", info);
+    loop {}
+}
+
+#[cfg(test)]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    serial_println!("[failed]\n");
+    serial_println!("Error: {}\n", info);
+    exit_qemu(QemuExitCode::Failed);
     loop {}
 }
 
