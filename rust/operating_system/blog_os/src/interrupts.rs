8bdc1a9253c36246c1a06f8babc44d577a1654d1
Takayuki Sato <s.yukkiy@gmail.com>
2022-03-26 14:21:28 +0900

    feat: IDT lifecycle to static using lazy

--- rust/operating_system/blog_os/src/interrupts.rs
+++ rust/operating_system/blog_os/src/interrupts.rs
@@ -1,9 +1,18 @@
 use crate::println;
 use x86_64::structures::idt::{InterruptDescriptorTable, InterruptStackFrame};
 
+use lazy_static::lazy_static;
+
+lazy_static! {
+    static ref IDT: InterruptDescriptorTable = {
+        let mut idt = InterruptDescriptorTable::new();
+        idt.breakpoint.set_handler_fn(breakpoint_handler);
+        idt
+    };
+}
+
 pub fn init_idt() {
-    let mut idt = InterruptDescriptorTable::new();
-    idt.breakpoint.set_handler_fn(breakpoint_handler);
+    IDT.load();
 }
 
 extern "x86-interrupt" fn breakpoint_handler(stack_frame: InterruptStackFrame) {
