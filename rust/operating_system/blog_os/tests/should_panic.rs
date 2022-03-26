341c1cc44589109e035e155c7d5fd0bd19b28dec
Takayuki Sato <s.yukkiy@gmail.com>
2022-03-26 14:02:05 +0900

    feat: remove test runner for panic

--- rust/operating_system/blog_os/tests/should_panic.rs
+++ rust/operating_system/blog_os/tests/should_panic.rs
@@ -4,27 +4,17 @@
 #![test_runner(test_runner)]
 #![reexport_test_harness_main = "test_main"]
 
-use blog_os::serial_print;
+use blog_os::{exit_qemu, serial_print, serial_println, QemuExitCode};
+use core::panic::PanicInfo;
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    test_main();
+    should_fail();
+    serial_println!("[test did not panic]");
+    exit_qemu(QemuExitCode::Failed);
 
     loop {}
 }
-
-pub fn test_runner(tests: &[&dyn Fn()]) {
-    serial_println!("Running {} tests", tests.len());
-    for test in tests {
-        test();
-        serial_println!("[test did not panic]");
-        exit_qemu(QemuExitCode::Failed);
-    }
-    exit_qemu(QemuExitCode::Success);
-}
-
-use blog_os::{exit_qemu, serial_println, QemuExitCode};
-use core::panic::PanicInfo;
 
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
@@ -33,7 +23,6 @@ fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
 
-#[test_case]
 fn should_fail() {
     serial_print!("should_panic::should_fail...\t");
     assert_eq!(0, 1);
