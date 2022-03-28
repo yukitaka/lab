7b67b0228477181b7485a7795e727635d7ad8e42
Takayuki Sato <s.yukkiy@gmail.com>
2022-03-29 00:31:44 +0900

    feat: output level3 table

--- rust/operating_system/blog_os/src/main.rs
+++ rust/operating_system/blog_os/src/main.rs
@@ -7,6 +7,7 @@
 use blog_os::println;
 use bootloader::{entry_point, BootInfo};
 use core::panic::PanicInfo;
+use x86_64::structures::paging::PageTable;
 
 entry_point!(kernel_main);
 
@@ -23,6 +24,17 @@ fn kernel_main(boot_info: &'static BootInfo) -> ! {
     for (i, entry) in l4_table.iter().enumerate() {
         if !entry.is_unused() {
             println!("L4 Entry {}: {:?}", i, entry);
+
+            let phys = entry.frame().unwrap().start_address();
+            let virt = phys.as_u64() + boot_info.physical_memory_offset;
+            let ptr = VirtAddr::new(virt).as_mut_ptr();
+            let l3_table: &PageTable = unsafe { &*ptr };
+
+            for (i, entry) in l3_table.iter().enumerate() {
+                if !entry.is_unused() {
+                    println!("  L3 Entry {}: {:?}", i, entry);
+                }
+            }
         }
     }
 
