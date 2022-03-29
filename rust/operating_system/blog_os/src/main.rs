45149354ed41d87ee1015b16934ad730d982de9a
Takayuki Sato <s.yukkiy@gmail.com>
2022-03-30 00:28:05 +0900

    feat: create example mapping

--- rust/operating_system/blog_os/src/main.rs
+++ rust/operating_system/blog_os/src/main.rs
@@ -12,27 +12,21 @@ entry_point!(kernel_main);
 
 fn kernel_main(boot_info: &'static BootInfo) -> ! {
     use blog_os::memory;
-    use x86_64::structures::paging::Translate;
+    use x86_64::structures::paging::Page;
     use x86_64::VirtAddr;
 
     println!("Hello World{}", "!");
     blog_os::init();
 
     let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
-    let mapper = unsafe { memory::init(phys_mem_offset) };
+    let mut mapper = unsafe { memory::init(phys_mem_offset) };
+    let mut frame_allocator = memory::EmptyFrameAllocator;
 
-    let addresses = [
-        0xb8000,
-        0x201008,
-        0x0100_0020_1a10,
-        boot_info.physical_memory_offset,
-    ];
+    let page = Page::containing_address(VirtAddr::new(0));
+    memory::create_example_mapping(page, &mut mapper, &mut frame_allocator);
 
-    for &address in &addresses {
-        let virt = VirtAddr::new(address);
-        let phys = mapper.translate_addr(virt);
-        println!("{:?} -> {:?}", virt, phys);
-    }
+    let page_ptr: *mut u64 = page.start_address().as_mut_ptr();
+    unsafe { page_ptr.offset(400).write_volatile(0x_f021_f077_f065_f04e) };
 
     #[cfg(test)]
     test_main();
--- rust/operating_system/blog_os/src/memory.rs
+++ rust/operating_system/blog_os/src/memory.rs
@@ -1,6 +1,32 @@
-use x86_64::structures::paging::page_table::FrameError;
-use x86_64::structures::paging::{OffsetPageTable, PageTable};
-use x86_64::{PhysAddr, VirtAddr};
+use x86_64::{
+    structures::paging::{
+        page_table::FrameError, FrameAllocator, Mapper, OffsetPageTable, Page, PageTable,
+        PhysFrame, Size4KiB,
+    },
+    PhysAddr, VirtAddr,
+};
+
+pub struct EmptyFrameAllocator;
+
+unsafe impl FrameAllocator<Size4KiB> for EmptyFrameAllocator {
+    fn allocate_frame(&mut self) -> Option<PhysFrame<Size4KiB>> {
+        None
+    }
+}
+
+pub fn create_example_mapping(
+    page: Page,
+    mapper: &mut OffsetPageTable,
+    frame_allocator: &mut impl FrameAllocator<Size4KiB>,
+) {
+    use x86_64::structures::paging::PageTableFlags as Flags;
+
+    let frame = PhysFrame::containing_address(PhysAddr::new(0xb8000));
+    let flags = Flags::PRESENT | Flags::WRITABLE;
+
+    let map_to_result = unsafe { mapper.map_to(page, frame, flags, frame_allocator) };
+    map_to_result.expect("map_to failed").flush();
+}
 
 pub unsafe fn init(physical_memory_offset: VirtAddr) -> OffsetPageTable<'static> {
     let level_4_table = active_level_4_table(physical_memory_offset);
