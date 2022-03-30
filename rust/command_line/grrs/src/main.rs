b768d6db9d08755d6c90cb6fb55469bbc70e6626
Takayuki Sato <s.yukkiy@gmail.com>
2022-03-31 00:37:45 +0900

    feat: handling error

--- rust/command_line/grrs/src/main.rs
+++ rust/command_line/grrs/src/main.rs
@@ -10,14 +10,14 @@ struct Cli {
     path: std::path::PathBuf,
 }
 
-fn main() {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args = Cli::parse();
-    let content = std::fs::read_to_string(&args.path)
-        .expect("could not read file");
+    let result = std::fs::read_to_string(&args.path);
+    let content = match result {
+        Ok(content) => { content },
+        Err(error) => { return Err(error.into()); }
+    };
 
-    for line in content.lines() {
-        if line.contains(&args.pattern) {
-            println!("{}", line);
-        }
-    }
+    println!("file content: {}", content);
+    Ok(())
 }
