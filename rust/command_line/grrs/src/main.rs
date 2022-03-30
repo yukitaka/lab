57596a187ab0bf16e7679e7ea000b64532d987e7
Takayuki Sato <s.yukkiy@gmail.com>
2022-03-31 00:27:45 +0900

    feat: using clap crate

--- rust/command_line/grrs/src/main.rs
+++ rust/command_line/grrs/src/main.rs
@@ -1,16 +1,17 @@
-#[derive(Debug)]
+use clap::Parser;
+
+/// Search for a pattern in a file and display the lines that contain it.
+#[derive(Parser, Debug)]
 struct Cli {
+    /// The pattern to look for
     pattern: String,
+    /// The path to the file to read
+    #[clap(parse(from_os_str))]
     path: std::path::PathBuf,
 }
 
 fn main() {
-    let pattern = std::env::args().nth(1).expect("no pattern given");
-    let path = std::env::args().nth(2).expect("no path given");
-    let args = Cli {
-        pattern,
-        path: std::path::PathBuf::from(path),
-    };
+    let args = Cli::parse();
 
     println!("{:?}", args);
 }
