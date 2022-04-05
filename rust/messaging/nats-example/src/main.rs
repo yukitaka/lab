6383bae32e5d07d01c749a1d749c35efd00dc0dd
Takayuki Sato <s.yukkiy@gmail.com>
2022-04-05 10:00:43 +0900

    feat: implement publish and subscribe

--- rust/messaging/nats-example/src/main.rs
+++ rust/messaging/nats-example/src/main.rs
@@ -1,3 +1,17 @@
 fn main() {
-    println!("Hello, world!");
+    let nc = nats::Options::new()
+        .with_name("nats example")
+        .connect("127.0.0.1");
+    println!("{:?}", &nc);
+
+    if let Ok(nc) = nc {
+        if let Err(err) = nc.publish("example", "message") {
+            println!("{}", err);
+        }
+        if let Ok(sub) = nc.subscribe("example") {
+            for msg in sub.messages() {
+                println!("Received a {:?}", msg);
+            }
+        }
+    }
 }
