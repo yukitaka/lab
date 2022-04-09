ce1902361bffa41fd7f326bb7b8a4dd0b414abde
Takayuki Sato <s.yukkiy@gmail.com>
2022-04-09 22:54:53 +0900

    feat: implement tracing subscriber

--- rust/web/axum_tutorial/print-request-response/src/main.rs
+++ rust/web/axum_tutorial/print-request-response/src/main.rs
@@ -1,11 +1,21 @@
 use axum::{routing::post, Router};
 use std::net::SocketAddr;
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 
 #[tokio::main]
 async fn main() {
+    tracing_subscriber::registry()
+        .with(tracing_subscriber::EnvFilter::new(
+            std::env::var("RUST_LOG")
+                .unwrap_or_else(|_| "print_request_response=debug,tower_http=debug".into()),
+        ))
+        .with(tracing_subscriber::fmt::layer())
+        .init();
+
     let app = Router::new().route("/", post(|| async move { "Hello from `POST/`" }));
 
     let addr = SocketAddr::from(([0, 0, 0, 0], 3000));
+    tracing::debug!("listening on {}", addr);
     axum::Server::bind(&addr)
         .serve(app.into_make_service())
         .await
