5d47d1af6503d667a9e45a90bb500f812147ba8c
Takayuki Sato <s.yukkiy@gmail.com>
2022-05-09 23:04:21 +0900

    feat: implement tests

--- rust/algorithm/gcd/src/main.rs
+++ rust/algorithm/gcd/src/main.rs
@@ -1,6 +1,5 @@
 extern crate core;
 
-use std::cmp::{max, min};
 use clap::{Command, Arg};
 
 fn main() {
@@ -20,7 +19,7 @@ fn main() {
     let x = extract(matches.value_of("x"));
     let y = extract(matches.value_of("y"));
 
-    println!("gcd({}, {}) = {}", x, y, gcd(max(x, y), min(x, y)));
+    println!("gcd({}, {}) = {}", x, y, gcd(x, y));
 }
 
 fn extract(a: Option<&str>) -> usize {
@@ -33,11 +32,38 @@ fn extract(a: Option<&str>) -> usize {
     }
 }
 
-fn gcd(max: usize, min: usize) -> usize {
-    let r = max % min;
+fn gcd(x: usize, y: usize) -> usize {
+    let r = x % y;
+    println!("{} % {} = {}", x, y, r);
     if r == 0 {
-        return min
+        return y
     }
 
-    gcd(min, r)
+    gcd(y, r)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_greatest_common_divisor() {
+        assert_eq!(26, gcd(78, 52));
+        assert_eq!(2, gcd(20, 2));
+        assert_eq!(8, gcd(24, 16));
+    }
+
+    #[test]
+    fn test_unordered_number() {
+        assert_eq!(26, gcd(52, 78));
+        assert_eq!(2, gcd(2, 20));
+        assert_eq!(8, gcd(16, 8));
+    }
+
+    #[test]
+    fn test_triple_number() {
+        assert_eq!(26, gcd(gcd(52, 78), 104));
+        assert_eq!(2, gcd(gcd(2, 20), 90));
+        assert_eq!(8, gcd(gcd(16, 8), 88));
+    }
 }
