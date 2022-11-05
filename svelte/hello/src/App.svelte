cdd3fab28ff48b354b45204636152084515035c5
Takayuki Sato <s.yukkiy@gmail.com>
2022-11-05 23:16:13 +0900

    feat: modify global css

--- svelte/hello/src/App.svelte
+++ svelte/hello/src/App.svelte
@@ -10,18 +10,14 @@
 <Home />
 
 <style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
+  :root {
+    font-family: -apple-system, BlinkMacSystemFont, 'Segos UI', Roboto, Oxygen,
+    Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    height: 100%;
   }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
+  :global(body) {
+    margin: 0;
+    height: 100%;
+    background-color: #fbfbfb;
   }
 </style>
