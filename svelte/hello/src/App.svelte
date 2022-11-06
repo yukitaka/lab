0ae25d88dbd9fa7e9d24931d27e98e57cf41af6e
Takayuki Sato <s.yukkiy@gmail.com>
2022-11-06 13:45:39 +0900

    feat: extract global scss

--- svelte/hello/src/App.svelte
+++ svelte/hello/src/App.svelte
@@ -3,21 +3,9 @@
   import Counter from './lib/Counter.svelte'
   import Home from '@/pages/Home.svelte'
   import Header from '@/components/Header.svelte'
+  import './global.scss'
 </script>
 
 <Header />
 
 <Home />
-
-<style>
-  :root {
-    font-family: -apple-system, BlinkMacSystemFont, 'Segos UI', Roboto, Oxygen,
-    Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-    height: 100%;
-  }
-  :global(body) {
-    margin: 0;
-    height: 100%;
-    background-color: #fbfbfb;
-  }
-</style>
