00aaf965e88833fbb446ad138dc913b3fe4417f0
Takayuki Sato <s.yukkiy@gmail.com>
2022-11-07 23:34:37 +0900

    feat: implement props title and content

--- svelte/hello/src/components/Note.svelte
+++ svelte/hello/src/components/Note.svelte
@@ -3,9 +3,22 @@
   import { faStar } from '@fortawesome/free-solid-svg-icons'
   import { createEventDispatcher } from 'svelte'
   import { formatDate } from '@/libs/utils'
+
+  export let title: string
+  export let content: string
+
+  const trimContent = (content: string) => {
+    if (content.length > 100) {
+      return `${content.substring(0, 100)}...`
+    }
+
+    return content
+  }
 </script>
 
 <div class="note-card">
+  <div class="title">{title}</div>
+  <div class="preview">{trimContent(content)}</div>
 </div>
 
 <style lang="scss">
@@ -19,5 +32,17 @@
     box-shadow: rgb(0 0 0 / 24%) 0px 3px 8px;
     display: grid;
     grid-template-rows: 1fr 4fr 2fr 1fr;
+
+    .title {
+      color: #6f6f6f;
+      font-weight: bold;
+      margin-bottom: 10px;
+    }
+
+    .preview {
+      color: #2f2f2f;
+      font-size: 15px;
+      word-break: break-word;
+    }
   }
 </style>
