ab98029a60c8ab55fd2e85f3159bfb9b9fbf107d
Takayuki Sato <s.yukkiy@gmail.com>
2022-11-18 22:58:27 +0900

    feat: add redirect

--- solid/my-app/src/routes/index.tsx
+++ solid/my-app/src/routes/index.tsx
@@ -1,5 +1,7 @@
 import { Title, useRouteData } from "solid-start";
 import Counter from "~/components/Counter";
+import { redirect } from "solid-start/server";
+import { getUser } from "~/db/session";
 
 export function routeData() {
   return createServerData$(async (_, { request }) => {
@@ -17,7 +19,8 @@ export default function Home() {
   const user = useRouteData<typeof routeData>();
 
   return (
-    <main>
+    <main class="w-full p-4 space-y-2">
+      <h1 class="font-bold text-3xl">Hello {user()?.username}</h1>
       <Title>Hello World</Title>
       <h1>Hello world!</h1>
       <Counter />
