96345e1d6d99de74e872492540da908e32bab526
Takayuki Sato <s.yukkiy@gmail.com>
2022-11-19 12:31:27 +0900

    feat: add basic forms

--- solid/my-app/src/routes/login.tsx
+++ solid/my-app/src/routes/login.tsx
@@ -1,13 +1,49 @@
+import { useParams } from "solid-start";
+import { FormError } from "solid-start/data";
 import { createServerAction$ } from "solid-start/server";
 
 export default function Login() {
+  const params = useParams();
+
   const [loggingIn, { Form }] = createServerAction$(async (form: FormData) => {
+    if (Object.values(fieldErrors).some(Boolean)) {
+      throw new FormError("Fields invalid", { fieldErrors, fields });
+    }
   });
 
   return (
     <main>
       <h1>Login</h1>
       <Form>
+        <input type="hidden" name="redirectTo" value={params.redirectTo ?? "/"} />
+        <fieldset>
+          <legend>Login or Register?</legend>
+          <label>
+            <input type="radio" name="loginType" value="login" checked={true} /> Login
+          </label>
+          <label>
+            <input type="radio" name="loginType" value="register" /> Register
+          </label>
+        </fieldset>
+        <div>
+          <label for="username-input">Username</label>
+          <input name="username" placeholder="name" />
+        </div>
+        <Show when={loggingIn.error?.fieldErrors?.username}>
+          <p role="alert">{loggingIn.error.fieldErrors.username}</p>
+        </Show>
+        <div>
+          <label for="password-input">Password</label>
+          <input name="password" type="password" placeholder="passw0rd" />
+        </div>
+        <Show when={loggingIn.error?.fieldErrors?.password}>
+          <p role="alert">{loggingIn.error.fieldErrors.password}</p>
+        </Show>
+        <Show when={loggingIn.error}>
+          <p role="alert" id="error-message">
+            {loggingIn.error.message}
+          </p>
+        </Show>
       </Form>
     </main>
   )
