0579138e77d938c251d2a86fda99605eda4b508d
Takayuki Sato <s.yukkiy@gmail.com>
2022-11-23 22:24:04 +0900

    feat: add logout

--- solid/my-app/src/routes/index.tsx
+++ solid/my-app/src/routes/index.tsx
@@ -1,6 +1,6 @@
 import { useRouteData } from "solid-start";
-import { redirect } from "solid-start/server";
-import { getUser } from "~/db/session";
+import { createServerAction$, createServerData$, redirect } from "solid-start/server";
+import { getUser, logout } from "~/db/session";
 
 export function routeData() {
   return createServerData$(async (_, { request }) => {
@@ -16,10 +16,16 @@ export function routeData() {
 
 export default function Home() {
   const user = useRouteData<typeof routeData>();
+  const [, { Form }] = createServerAction$((f: FormData, { request }) => logout(request));
 
   return (
     <main class="w-full p-4 space-y-2">
       <h1 class="font-bold text-3xl">Hello {user()?.username}</h1>
+      <Form>
+        <button name="logout" type="submit">
+          Logout
+        </button>
+      </Form>
     </main>
   );
 }
