3e905a6ed2d9924f39c9f648918937b4799f3a8b
Takayuki Sato <s.yukkiy@gmail.com>
2022-12-07 02:31:46 +0900

    feat: implement customers

--- kotlin/ktor-sample/src/main/kotlin/com/example/plugins/Routing.kt
+++ kotlin/ktor-sample/src/main/kotlin/com/example/plugins/Routing.kt
@@ -1,16 +1,12 @@
 package com.example.plugins
 
+import com.example.routes.customerRouting
 import io.ktor.server.routing.*
-import io.ktor.http.*
 import io.ktor.server.application.*
-import io.ktor.server.response.*
-import io.ktor.server.request.*
 
 fun Application.configureRouting() {
 
     routing {
-        get("/") {
-            call.respondText("Hello World!")
-        }
+        customerRouting()
     }
 }
