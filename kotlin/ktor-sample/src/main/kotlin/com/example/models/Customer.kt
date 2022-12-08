f0b1676a182eb9cb2a6a1af5d68d985a3c6cda33
Takayuki Sato <s.yukkiy@gmail.com>
2022-12-08 23:21:34 +0900

    feat: add a postgres support

--- kotlin/ktor-sample/src/main/kotlin/com/example/models/Customer.kt
+++ kotlin/ktor-sample/src/main/kotlin/com/example/models/Customer.kt
@@ -1,8 +1,17 @@
 package com.example.models
 
 import kotlinx.serialization.Serializable
+import org.jetbrains.exposed.sql.*
 
 @Serializable
 data class Customer(val id: String, val firstName: String, val lastName: String, val email: String)
+
+object Customers : Table() {
+    val id = integer("id").autoIncrement()
+    val firstName = varchar("First", 128)
+    val lastName = varchar("Last", 128)
+
+    override val primaryKey = PrimaryKey(id)
+}
 
 val customerStorage = mutableListOf<Customer>()
