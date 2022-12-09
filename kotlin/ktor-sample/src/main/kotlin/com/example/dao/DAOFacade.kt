a786f80ee71ab2987cce16e79bcc0fbbd1fd5675
Takayuki Sato <s.yukkiy@gmail.com>
2022-12-10 02:17:38 +0900

    feat: add interfaces of customers

--- kotlin/ktor-sample/src/main/kotlin/com/example/dao/DAOFacade.kt
+++ kotlin/ktor-sample/src/main/kotlin/com/example/dao/DAOFacade.kt
@@ -4,4 +4,8 @@ import com.example.models.*
 
 interface DAOFacade {
     suspend fun allCustomers(): List<Customer>
+    suspend fun customer(id: Int): Customer?
+    suspend fun addNewCustomer(firstName: String, lastName: String, email: String): Customer?
+    suspend fun editCustomer(id: Int, firstName: String, lastName: String, email: String): Boolean
+    suspend fun deleteCustomer(id: Int): Boolean
 }
--- kotlin/ktor-sample/src/main/kotlin/com/example/dao/DAOFacadeImpl.kt
+++ kotlin/ktor-sample/src/main/kotlin/com/example/dao/DAOFacadeImpl.kt
@@ -17,6 +17,22 @@ class DAOFacadeImpl : DAOFacade {
     override suspend fun allCustomers(): List<Customer> = dbQuery {
         Customers.selectAll().map(::resultRowToCustomer)
     }
+
+    override suspend fun customer(id: Int): Customer? {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun addNewCustomer(firstName: String, lastName: String, email: String): Customer? {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun editCustomer(id: Int, firstName: String, lastName: String, email: String): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun deleteCustomer(id: Int): Boolean {
+        TODO("Not yet implemented")
+    }
 }
 
 val dao: DAOFacade = DAOFacadeImpl().apply {
