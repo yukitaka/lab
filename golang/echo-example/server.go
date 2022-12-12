1d8cb429a7a239d03ed0f17f681f6480e6348f8b
Takayuki Sato <s.yukkiy@gmail.com>
2022-12-12 23:39:38 +0900

    feat: add example routing

--- golang/echo-example/server.go
+++ golang/echo-example/server.go
@@ -11,5 +11,11 @@ func main() {
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, World!")
 	})
+	e.GET("/users/:id", getUser)
 	e.Logger.Fatal(e.Start(":1323"))
+}
+
+func getUser(c echo.Context) error {
+	id := c.Param("id")
+	return c.String(http.StatusOK, id)
 }
