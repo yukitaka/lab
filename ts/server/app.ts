b4722a9a8266bf9e1a2bf4e57bd82dd7d1750c03
Takayuki Sato <s.yukkiy@gmail.com>
2022-12-23 19:51:52 +0900

    feat: implement http server

--- ts/server/app.ts
+++ ts/server/app.ts
@@ -1,1 +1,13 @@
-console.log("Hello! world");
+import * as http from "http";
+
+const port = 5000;
+
+const server = http.createServer(
+  (request, response) => {
+    response.end("Hello! world");
+  }
+);
+
+server.listen(port);
+
+console.log(`http://localhost:${port}`);
