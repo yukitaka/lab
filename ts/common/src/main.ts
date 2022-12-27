5b61aa13a07e7fecccf7c3e8dcd14e5eddd2a01e
Takayuki Sato <s.yukkiy@gmail.com>
2022-12-27 22:31:25 +0900

    feat: add type examples

--- ts/common/src/main.ts
+++ ts/common/src/main.ts
@@ -10,3 +10,22 @@ human.shakeHands()
 
 const taro = new Taro()
 taro.greeting()
+
+let list = ["this", "is", "a", "test"]
+list.push("!")
+console.log(list)
+
+list = list.map(item => item.toUpperCase())
+console.log(list)
+
+let message = list.reduce((acc, value) => `${acc} ${value}`)
+console.log(message)
+
+function getFormattedValue(value: number, unit: string | null = "pt") {
+  const _value = value.toFixed(2)
+  if (unit === null) return `${_value}`
+  return `${_value} ${unit.toUpperCase()}`
+}
+
+console.log(getFormattedValue(100))
+console.log(getFormattedValue(100, 'kg'))
