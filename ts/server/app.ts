282394c0209afdf1881a6d419c5d6ed8163c3c05
Takayuki Sato <s.yukkiy@gmail.com>
2022-12-29 22:12:45 +0900

    feat: implement to send from producer example

--- ts/server/app.ts
+++ ts/server/app.ts
@@ -1,11 +1,28 @@
 import * as http from "http";
 import { port } from "./config";
+import { KafkaClient, HighLevelProducer } from "kafka-node";
 
 const server = http.createServer(
   (request, response) => {
+    const client = new  KafkaClient({kafkaHost: "127.0.0.1:9093"});
+    const producer = new HighLevelProducer(client);
+    const payloads = [
+      { topic: 'test-topic', messages: ['hello', 'world'] }
+    ];
+    producer.on("ready", () => {
+      producer.send(payloads, function (err: any, data: any) {
+        console.log(err);
+        console.log(data);
+      });
+    });
+    producer.on('error', function(err: any) {
+      console.log("====");
+      console.log(err)
+    });
     response.end("Hello! world");
   }
 );
+
 
 server.listen(port);
 
