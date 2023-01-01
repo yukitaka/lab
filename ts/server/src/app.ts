d6a50edbe743512ba19ce349375bfb1603210616
Takayuki Sato <s.yukkiy@gmail.com>
2023-01-01 20:25:09 +0900

    feat: extract kafka producer api

--- ts/server/src/app.ts
+++ ts/server/src/app.ts
@@ -1,12 +1,15 @@
 import express from "express";
+import bodyParser from "body-parser"
 import { port } from "./config";
 import * as handlers from "./handlers"
 
 const app = express()
+app.use(bodyParser.json())
+
 const host = "localhost"
 
 app.get("/", (req, res) => handlers.index(req, res))
-
+app.post("/send", (req, res) => handlers.send(req, res))
 app.listen(port, host, () => {
   console.log(`Running on http://${host}:${port}`)
 })
--- ts/server/src/handlers.ts
+++ ts/server/src/handlers.ts
@@ -2,17 +2,22 @@ import express from "express"
 import { KafkaClient, HighLevelProducer } from "kafka-node";
 
 export function index(req: express.Request, res: express.Response) {
-    const client = new  KafkaClient({kafkaHost: "127.0.0.1:9093"});
-    const producer = new HighLevelProducer(client);
-    const payloads = [
-      { topic: 'test-topic', messages: ['hello', 'world'] }
-    ];
-    producer.on("ready", () => {
-      producer.send(payloads, function (err: any, data: any) {
-        console.log(err);
-        console.log(data);
-      });
-    });
+    res.send(JSON.stringify({"msg": "Hello World!"}))
+}
 
-    res.send(JSON.stringify(payloads))
+export function send(req: express.Request, res: express.Response) {
+  console.log(req.body)
+  const payloads = [
+    { topic: 'test-topic', messages: req.body.msg }
+  ];
+  const client = new  KafkaClient({kafkaHost: "127.0.0.1:9093"});
+  const producer = new HighLevelProducer(client);
+  producer.on("ready", () => {
+    producer.send(payloads, function (err: any, data: any) {
+      console.log(err);
+      console.log(data);
+    });
+  });
+
+  res.send(JSON.stringify(payloads))
 }
