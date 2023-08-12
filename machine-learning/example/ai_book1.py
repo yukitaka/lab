62ef247f01492589a7bec7fc45767f770919a3a0
Takayuki Sato <s.yukkiy@gmail.com>
2023-08-13 01:03:54 +0900

    feat: confirm labels

--- machine-learning/example/ai_book1.py
+++ machine-learning/example/ai_book1.py
@@ -9,12 +9,28 @@ import matplotlib.pyplot as plot
 
 (train_data, train_labels), (test_data, test_labels) = boston_housing.load_data()
 
-#print(train_data.shape)
-#print(train_labels.shape)
-#print(test_data.shape)
+def count_shapes(train_data, train_labels, test_data):
+    print("Count shapes")
+    print(train_data.shape)
+    print(train_labels.shape)
+    print(test_data.shape)
 
-column_names = ['CRIM', 'ZN', 'INDUS', 'CHAS', 'NOX', 'RM', 'AGE', 'DIS', 'RAD', 'TAX', 'PTRATIO', 'B', 'LSTAT']
-df = pd.DataFrame(train_data, columns=column_names)
-df.head()
 
-print(df)
+def view_tables(train_data):
+    print("View tables")
+    column_names = ['CRIM', 'ZN', 'INDUS', 'CHAS', 'NOX', 'RM', 'AGE', 'DIS', 'RAD', 'TAX', 'PTRATIO', 'B', 'LSTAT']
+    df = pd.DataFrame(train_data, columns=column_names)
+    df.head()
+
+    print(df)
+
+
+def confirm_labels(train_labels):
+    print("Confirm labels")
+    print(train_labels[0:10])
+
+
+count_shapes(train_data, train_labels, test_data)
+view_tables(train_data)
+confirm_labels(train_labels)
+
