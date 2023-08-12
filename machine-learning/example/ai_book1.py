9f5106c40e953e059b54bab0f5ef0fe22054bd3f
Takayuki Sato <s.yukkiy@gmail.com>
2023-08-13 00:49:39 +0900

    feat: load data

---
+++ machine-learning/example/ai_book1.py
@@ -0,0 +1,14 @@
+from tensorflow.keras.datasets import boston_housing
+from tensorflow.keras.layers import Activation, Dense, Dropout
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.callbacks import EarlyStopping
+from tensorflow.keras.optimizers import Adam
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plot
+
+(train_data, train_labels), (test_data, test_labels) = boston_housing.load_data()
+
+print(train_data.shape)
+print(train_labels.shape)
+print(test_data.shape)
