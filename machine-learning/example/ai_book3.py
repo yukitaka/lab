5baa61aa4ff10f1035e08d220d1a23cd302de69c
Takayuki Sato <s.yukkiy@gmail.com>
2023-08-16 00:35:46 +0900

    feat: preview data

--- machine-learning/example/ai_book3.py
+++ machine-learning/example/ai_book3.py
@@ -7,3 +7,19 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 (train_images, train_labels), (test_images, test_labels) = cifar10.load_data()
+
+def data_preview(train_images, train_labels, test_images, test_labels):
+    print(train_images.shape)
+    print(train_labels.shape)
+    print(test_images.shape)
+    print(test_labels.shape)
+
+    for i in range(10):
+        plt.subplot(2, 5, i+1)
+        plt.imshow(train_images[i])
+    plt.show()
+
+    print(train_labels[0:10])
+
+
+data_preview(train_images, train_labels, test_images, test_labels)
