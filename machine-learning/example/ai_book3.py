0151a43a64f376ce1100198c56d7b8d7d7356d96
Takayuki Sato <s.yukkiy@gmail.com>
2023-08-16 00:54:29 +0900

    feat: create a model

--- machine-learning/example/ai_book3.py
+++ machine-learning/example/ai_book3.py
@@ -32,3 +32,20 @@ test_images = test_images.astype('float32')/255.0
 train_labels = to_categorical(train_labels, 10)
 test_labels = to_categorical(test_labels, 10)
 
+model = Sequential()
+model.add(Conv2D(32, (3, 3), activation='relu', padding='same', input_shape=(32, 32, 3)))
+model.add(Conv2D(32, (3, 3), activation='relu', padding='same'))
+model.add(MaxPool2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
+
+model.add(Conv2D(64, (3, 3), activation='relu', padding='same'))
+model.add(Conv2D(64, (3, 3), activation='relu', padding='same'))
+model.add(MaxPool2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
+
+model.add(Flatten())
+model.add(Dense(512, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(10, activation='softmax'))
+
+model.compile(loss='categorical_crossentropy', optimizer=Adam(learning_rate=0.001), metrics=['acc'])
