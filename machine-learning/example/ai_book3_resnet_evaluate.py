b6c398f229a155cb24e69559d18996456dce02c9
Takayuki Sato <s.yukkiy@gmail.com>
2023-08-20 23:40:53 +0900

    feat: predict

--- machine-learning/example/ai_book3_resnet_evaluate.py
+++ machine-learning/example/ai_book3_resnet_evaluate.py
@@ -3,6 +3,8 @@ from tensorflow.keras.models import Model
 from tensorflow.keras.utils import to_categorical
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from tensorflow import keras
+import numpy as np
+import matplotlib.pyplot as plt
 
 model = keras.models.load_model('resnet.keras')
 
@@ -20,3 +22,14 @@ test_loss, test_acc = model.evaluate(
         steps=10)
 
 print('loss: {:.3f}\nacc: {:.3f}'.format(test_loss, test_acc))
+
+for i in range(10):
+    plt.subplot(2, 5, i+1)
+    plt.imshow(test_images[i])
+plt.show()
+
+test_predictions = model.predict(
+        test_gen.flow(test_images[0:10], shuffle = False, batch_size=1), steps=10)
+test_predictions = np.argmax(test_predictions, axis=1)
+labels = ['airplane', 'automobile', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck']
+print([labels[n] for n in test_predictions])
