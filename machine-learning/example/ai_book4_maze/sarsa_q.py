b43e4bad308212241b11a5b922ee3c6baa34e74b
Takayuki Sato <s.yukkiy@gmail.com>
2023-08-24 00:18:35 +0900

    feat: create a sarsa q policy

---
+++ machine-learning/example/ai_book4_maze/sarsa_q.py
@@ -0,0 +1,18 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import animation
+from IPython.display import HTML
+import maze as mz
+
+fig, circle, theta_0 = mz.prepare_maze()
+
+def get_pi(theta):
+    [m, n] = theta.shape
+    pi = np.zeros((m, n))
+    for i in range(0, m):
+        pi[i, :] = theta[i, :] / np.nansum(theta[i, :])
+    pi = np.nan_to_num(pi)
+    return pi
+
+pi_0 = get_pi(theta_0)
+print(pi_0)
