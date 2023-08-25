3f57655d249010464ed5ef48194452da6b9082ec
Takayuki Sato <s.yukkiy@gmail.com>
2023-08-25 23:36:59 +0900

    feat: implement Q learning

--- machine-learning/example/ai_book4_maze/sarsa_q.py
+++ machine-learning/example/ai_book4_maze/sarsa_q.py
@@ -37,3 +37,12 @@ def sarsa(s, a, r, s_next, a_next, Q):
         Q[s, a] = Q[s, a] + eta * (r + gamma * Q[s_next, a_next] - Q[s, a])
     return Q
 
+def q_learning(s, a, r, s_next, a_next, Q):
+    eta = 0.1
+    gamma = 0.9
+
+    if s_next == 8:
+        Q[s, a] = Q[s, a] + eta * (r -Q[s, a])
+    else:
+        Q[s, a] = Q[s, a] + eta * (r + gamma * np.nanmax(Q[s_next, :]) - Q[s, a])
+    return Q
