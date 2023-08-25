2d22d0ccc8e065dd3aff04db6c63fcb397a8d63e
Takayuki Sato <s.yukkiy@gmail.com>
2023-08-25 23:53:28 +0900

    feat: play and learn the maze

--- machine-learning/example/ai_book4_maze/sarsa_q.py
+++ machine-learning/example/ai_book4_maze/sarsa_q.py
@@ -27,22 +27,55 @@ def get_a(s, Q, epsilon, pi_0):
     else:
         return np.nanargmax(Q[s])
 
-def sarsa(s, a, r, s_next, a_next, Q):
-    eta = 0.1
-    gamma = 0.9
-
+def sarsa(s, a, r, s_next, a_next, Q, eta, gamma):
     if s_next == 8:
         Q[s, a] = Q[s, a] + eta * (r - Q[s, a])
     else:
         Q[s, a] = Q[s, a] + eta * (r + gamma * Q[s_next, a_next] - Q[s, a])
     return Q
 
-def q_learning(s, a, r, s_next, a_next, Q):
-    eta = 0.1
-    gamma = 0.9
-
+def q_learning(s, a, r, s_next, a_next, Q, eta, gamma):
     if s_next == 8:
         Q[s, a] = Q[s, a] + eta * (r -Q[s, a])
     else:
         Q[s, a] = Q[s, a] + eta * (r + gamma * np.nanmax(Q[s_next, :]) - Q[s, a])
     return Q
+
+def play(Q, epsilon, eta, gamma, pi):
+    s = 0
+    a = a_next = get_a(s, Q, epsilon, pi)
+    s_a_history = [[0, np.nan]]
+
+    while True:
+        a = a_next
+        s_next = mz.get_s_next(s, a)
+
+        s_a_history[-1][1] = a
+        s_a_history.append([s_next, np.nan])
+
+        if s_next == 8:
+            r = 1
+            a_next = np.nan
+        else:
+            r = 0
+            a_next = get_a(s_next, Q, epsilon, pi)
+
+        Q = sarsa(s, a, r, s_next, a_next, Q, eta, gamma)
+
+        if s_next == 8:
+            break
+        else:
+            s = s_next
+
+    return [s_a_history, Q]
+
+eta = 0.1
+gamma = 0.9
+epsilon = 0.5
+
+for episode in range(10):
+    epsilon = epsilon / 2
+
+    [s_a_history, Q] = play(Q, epsilon, eta, gamma, pi_0)
+
+    print('episode: {}, step: {}'.format(episode, len(s_a_history)-1))
