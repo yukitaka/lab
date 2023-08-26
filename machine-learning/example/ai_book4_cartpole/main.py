9ffc07145af0b48d8f0ded2e51d2e7158f60a873
Takayuki Sato <s.yukkiy@gmail.com>
2023-08-26 20:31:11 +0900

    feat: implement a memory

--- machine-learning/example/ai_book4_cartpole/main.py
+++ machine-learning/example/ai_book4_cartpole/main.py
@@ -28,9 +28,26 @@ class QNetwork:
 
         self.model.compile(loss=Huber(delta=100.0), optimizer=Adam(learning_rate=0.001))
 
+class Memory():
+    def __init__(self, memory_size):
+        self.buffer = deque(maxlen=memory_size)
+
+    def add(self, experience):
+        self.buffer.append(experience)
+
+    def sample(self, batch_size):
+        idx = np.random.choice(np.arrange(len(self.buffer)), size=batch_size, replace=False)
+        return [self.buffer[i] for i in idx]
+
+    def __len__(self):
+        return len(self.buffer)
+
+
 env = gym.make('CartPole-v0')
 state_size = env.observation_space.shape[0]
 action_size = env.action_space.n
 
 main_qn = QNetwork(state_size, action_size)
 target_qn = QNetwork(state_size, action_size)
+memory = Memory(MEMORY_SIZE)
+
