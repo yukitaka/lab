a7dadf8b6410fde1f08c83233a04a8466461d04d
Takayuki Sato <s.yukkiy@gmail.com>
2023-08-27 02:46:55 +0900

    feat: implement episode loop

--- machine-learning/example/ai_book4_cartpole/main.py
+++ machine-learning/example/ai_book4_cartpole/main.py
@@ -36,7 +36,7 @@ class Memory():
         self.buffer.append(experience)
 
     def sample(self, batch_size):
-        idx = np.random.choice(np.arrange(len(self.buffer)), size=batch_size, replace=False)
+        idx = np.random.choice(np.arange(len(self.buffer)), size=batch_size, replace=False)
         return [self.buffer[i] for i in idx]
 
     def __len__(self):
@@ -51,3 +51,71 @@ main_qn = QNetwork(state_size, action_size)
 target_qn = QNetwork(state_size, action_size)
 memory = Memory(MEMORY_SIZE)
 
+state = env.reset()
+state = np.reshape(state[0], [1, state_size])
+
+total_step = 0
+success_count = 0
+for episode in range(1, NUM_EPISODES+1):
+    step = 0
+    epsilon = 0
+
+    target_qn.model.set_weights(main_qn.model.get_weights())
+
+    for _ in range(1, MAX_STEPS+1):
+        step += 1
+        total_step += 1
+
+        epsilon = E_STOP + (E_START - E_STOP)*np.exp(-E_DECAY_RATE*total_step)
+        if epsilon > np.random.rand():
+            action = env.action_space.sample()
+        else:
+            action = np.argmax(main_qn.model.predict(state)[0])
+
+        next_state, _, terminated, truncated, _ = env.step(action)
+        next_state = np.reshape(next_state, [1, state_size])
+
+        if terminated or truncated:
+            if step >= 190:
+                success_count += 1
+                reward = 1
+            else:
+                success_count = 0
+                reward = 0
+
+            next_state = np.zeros(state.shape)
+
+            if step > WARMUP:
+                memory.add((state, action, reward, next_state))
+        else:
+            reward = 0
+            if step > WARMUP:
+                memory.add((state, action, reward, next_state))
+
+            state = next_state
+
+        if len(memory) >= BATCH_SIZE:
+            inputs = np.zeros((BATCH_SIZE, 4))
+            targets = np.zeros((BATCH_SIZE, 2))
+
+            minibatch = memory.sample(BATCH_SIZE)
+
+            for i, (state_b, action_b, reward_b, next_state_b) in enumerate(minibatch):
+                inputs[i] = state_b
+                if not (next_state_b == np.zeros(state_b.shape)).all(axis=1):
+                    target = reward_b + GAMMA * np.amax(target_qn.model.predict(next_state_b)[0])
+                else:
+                    target = reward_b
+                targets[i] = main_qn.model.predict(state_b)
+                targets[i][action_b] = target
+            main_qn.model.fit(inputs, targets, epochs=1, verbose=0)
+        if terminated or truncated:
+            break
+
+    print('episode: {}, stap: {}, epsilon: {:.4f}'.format(episode, step, epsilon))
+
+    if success_count >= 5:
+        break
+
+    state = env.reset()
+    state = np.reshape(state[0], [1, state_size])
