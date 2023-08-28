ad9e73d3c3e01c75fef5d9ff6a9f57d105b033f3
Takayuki Sato <s.yukkiy@gmail.com>
2023-08-28 23:58:06 +0900

    feat: create a state

---
+++ machine-learning/example/ai_book5_three/main.py
@@ -0,0 +1,23 @@
+import random
+
+class State:
+    def __init__(self, pieces=None, enemy_pieces=None):
+        self.pieces = pieces if pieces != None else [0] * 9
+        self.enemy_pieces = enemy_pieces if enemy_pieces != None else [0] * 9
+
+    def piece_count(self, pieces):
+        count = 0
+        for i in pieces:
+            if i == 1:
+                count += 1
+            return count
+
+    def is_lose(self):
+        def if_comp(x, y, dx, dy):
+            for k in range(3):
+                if y < 0 or 2 < y or x < 0 or 2 < x or self.enemy_pieces[x+y*3] == 0:
+                    return False
+                x, y = x+dx, y+dy
+            return True
+
+
