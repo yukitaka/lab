8e963f5b3b818decd695650c9c73e7e51f0d7c61
Takayuki Sato <s.yukkiy@gmail.com>
2023-08-30 01:12:06 +0900

    feat: implement a theory of minimax

--- machine-learning/example/ai_book5_three/main.py
+++ machine-learning/example/ai_book5_three/main.py
@@ -68,12 +68,49 @@ def random_action(state):
     return legal_actions[random.randint(0, len(legal_actions)-1)]
 
 
+def mini_max(state):
+    if state.is_lose():
+        return -1
+
+    if state.is_draw():
+        return 0
+
+    best_score = -float('inf')
+    for action in state.legal_actions():
+        score = -mini_max(state.next(action))
+        if score > best_score:
+            best_score = score
+
+    return best_score
+
+
+def mini_max_action(state):
+    best_action = 0
+    best_score = -float('inf')
+    str = ['', '']
+    for action in state.legal_actions():
+        score = -mini_max(state.next(action))
+        if score > best_score:
+            best_action = action
+            best_score = score
+
+        str[0] = '{}{:2d},'.format(str[0], action)
+        str[1] = '{}{:2d},'.format(str[1], score)
+    print('action:', str[0], '\nscore: ', str[1], '\n')
+
+    return best_action
+
+
 state = State()
 while True:
     if state.is_done():
         break;
 
-    action = random_action(state)
+    if state.is_first_player():
+        action = mini_max_action(state)
+    else:
+        action = random_action(state)
+
     state = state.next(action)
 
     print(state)
