67aa7ccc2a2a8a212479d347b6eb77be7886a719
Takayuki Sato <s.yukkiy@gmail.com>
2023-09-01 00:21:29 +0900

    feat: implement a theory of monte carlo

--- machine-learning/example/ai_book5_three/main.py
+++ machine-learning/example/ai_book5_three/main.py
@@ -157,17 +157,67 @@ def mini_max_action(state):
     return best_action
 
 
-state = State()
-while True:
-    if state.is_done():
-        break;
+def playout(state):
+    if state.is_lose():
+        return -1
 
-    if state.is_first_player():
-        action = alpha_beta_action(state)
-    else:
-        action = mini_max_action(state)
+    if state.is_draw():
+        return 0
 
-    state = state.next(action)
+    return -playout(state.next(random_action(state)))
 
-    print(state)
-    print("")
+
+def mcs_action(state):
+    legal_actions = state.legal_actions()
+    values = [0] * len(legal_actions)
+    for i, action in enumerate(legal_actions):
+        for _ in range(10):
+            values[i] += -playout(state.next(action))
+    return legal_actions[argmax(values)]
+
+
+def argmax(collection, key=None):
+    return collection.index(max(collection))
+
+
+EP_GAME_COUNT = 100
+
+def first_player_point(ended_state):
+    if ended_state.is_lose():
+        return 0 if ended_state.is_first_player() else 1
+    return 0.5
+
+
+def play(next_actions):
+    state = State()
+    while True:
+        if state.is_done():
+            break;
+
+        next_action = next_actions[0] if state.is_first_player() else next_actions[1]
+        action = next_action(state)
+
+        state = state.next(action)
+
+    return first_player_point(state)
+
+
+def evaluate_algorithm_of(label, next_actions):
+    total_point = 0
+    for i in range(EP_GAME_COUNT):
+        if i % 2  == 0:
+            total_point += play(next_actions)
+        else:
+            total_point += 1 - play(list(reversed(next_actions)))
+
+        print('\rEvaluate {}/{}'.format(i + 1, EP_GAME_COUNT))
+    print('')
+
+    average_point = total_point / EP_GAME_COUNT
+    print(label.format(average_point))
+
+next_actions = (mcs_action, random_action)
+evaluate_algorithm_of('VS_Random {:.3f}', next_actions)
+
+next_actions = (mcs_action, alpha_beta_action)
+evaluate_algorithm_of('VS_AlphaBeta {:.3f}', next_actions)
