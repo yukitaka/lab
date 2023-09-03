142414ef25c9fe98ce771b09afdb4ec6ffdcbf93
Takayuki Sato <s.yukkiy@gmail.com>
2023-09-04 00:52:25 +0900

    implement to detect by montecarlo

--- machine-learning/example/ai_book6_alpha/pv_mcts.py
+++ machine-learning/example/ai_book6_alpha/pv_mcts.py
@@ -11,7 +11,7 @@ PV_EVALUATE_COUNT = 50
 def predict(model, state):
     a, b, c = DN_INPUT_SHAPE
     x = np.array([state.pieces, state.enemy_pieces])
-    x = x.rehsape(c, a, b).transpose(1, 2, 0).reshape(1, a, b, c)
+    x = x.reshape(c, a, b).transpose(1, 2, 0).reshape(1, a, b, c)
 
     y = model.predict(x, batch_size=1)
 
@@ -30,3 +30,89 @@ def nodes_to_scores(nodes):
 
     return scores
 
+
+def pv_mcts_scores(model, state, temperature):
+    class node:
+        def __init__(self, state, p):
+            self.state = state
+            self.p = p
+            self.w = 0
+            self.n = 0
+            self.child_nodes = None
+
+        def evaluate(self):
+            if self.state.is_done():
+                value = -1 if self.state.is_lose() else 0
+                self.w += value
+                self.n += 1
+
+                return value
+
+            if not self.child_nodes:
+                policies, value = predict(model, self.state)
+                self.w += value
+                self.n += 1
+
+                self.child_nodes = []
+                for action, policy in zip(self.state.legal_actions(), policies):
+                    self.child_nodes.append(node(self.state.next(action), policy))
+                return value
+            else:
+                value = -self.next_child_node().evaluate()
+                self.w += value
+                self.n += 1
+
+                return value
+
+        def next_child_node(self):
+            C_PUCT = 1.0
+            t = sum(nodes_to_scores(self.child_nodes))
+            pucb_values = []
+            for child_node in self.child_nodes:
+                pucb_values.append((-child_node.w / child_node.n if child_node.n else 0.0) + C_PUCT * child_node.p * sqrt(t) / (1 + child_node.n))
+
+            return self.child_nodes[np.argmax(pucb_values)]
+
+    root_node = node(state, 0)
+
+    for _ in range(PV_EVALUATE_COUNT):
+        root_node.evaluate()
+
+    scores = nodes_to_scores(root_node.child_nodes)
+    if temperature == 0:
+        action = np.argmax(scores)
+        scores = np.zeros(len(scores))
+        scores[action] = 1
+    else:
+        scores = boltzman(scores, temperature)
+
+    return scores
+
+
+def pv_mcts_action(model, temperature=0):
+    def pv_mcts_action(state):
+        scores = pv_mcts_scores(model, state, temperature)
+        return np.random.choice(state.legal_actions(), p=scores)
+    return pv_mcts_action
+
+
+def boltzman(xs, temperature):
+    xs = [x ** (1 / temperature) for x in xs]
+    return [x / sum(xs) for x in xs]
+
+
+if __name__ == '__main__':
+    path = sorted(Path('./model').glob('*.keras'))[-1]
+    model = load_model(str(path))
+
+    state = State()
+    next_action = pv_mcts_action(model, 1.0)
+
+    while True:
+        if state.is_done():
+            break
+
+        action = next_action(state)
+        state = state.next(action)
+
+        print(state)
