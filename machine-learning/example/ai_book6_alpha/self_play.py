c3813edb157c03a42c7153cc5c059e1782013ccc
Takayuki Sato <s.yukkiy@gmail.com>
2023-09-05 01:18:31 +0900

    feat: implement to play

--- machine-learning/example/ai_book6_alpha/self_play.py
+++ machine-learning/example/ai_book6_alpha/self_play.py
@@ -27,3 +27,26 @@ def write_data(history):
     with open(path, mode='wb') as f:
         pickle.dump(history, f)
 
+
+def play(model):
+    history = []
+    state = State()
+
+    while True:
+        if state.is_done():
+            break
+
+        scores = pv_mcts_scores(model, state, SP_TEMPERATURE)
+        policies = [0] * DN_OUTPUT_SIZE
+        for action, policy in zip(state.legal_actions(), scores):
+            policies[action] = policy
+        history.append([[state.pieces, state.enemy_pieces], policies, None])
+
+        action = np.random.choice(state.legal_actions(), p=scores)
+        state = state.next(action)
+
+    value = first_player_value(state)
+    for i in range(len(history)):
+        history[i][2] = value
+        value = -value
+    return history
