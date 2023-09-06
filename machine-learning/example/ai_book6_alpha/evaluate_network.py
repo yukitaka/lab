ea7abb33986a578ed47599e7d43de3ddcad30873
Takayuki Sato <s.yukkiy@gmail.com>
2023-09-07 00:54:24 +0900

    feat: evaluate network

--- machine-learning/example/ai_book6_alpha/evaluate_network.py
+++ machine-learning/example/ai_book6_alpha/evaluate_network.py
@@ -23,7 +23,7 @@ def play(next_actions):
         if state.is_done():
             break
 
-        next_action = next_action[0] if state.is_first_palyer() else next_actions[1]
+        next_action = next_actions[0] if state.is_first_player() else next_actions[1]
         action = next_action(state)
 
         state = state.next(action)
@@ -32,6 +32,42 @@ def play(next_actions):
 
 
 def update_best_player():
-    copy('./model/latest.keras', './model/latest.keras')
+    copy('./model/latest.keras', './model/best.keras')
     print('Change BestPlayer')
+
+
+def evaluate_network():
+    model0 = load_model('./model/latest.keras')
+    model1 = load_model('./model/best.keras')
+
+    next_action0 = pv_mcts_action(model0, EN_TEMPERATURE)
+    next_action1 = pv_mcts_action(model1, EN_TEMPERATURE)
+    next_actions = (next_action0, next_action1)
+
+    total_point = 0
+    for i in range(EN_GAME_COUNT):
+        if i % 2 == 0:
+            total_point += play(next_actions)
+        else:
+            total_point += 1 - play(list(reversed(next_actions)))
+
+        print('\rEvaluate {}/{}'.format(i + 1, EN_GAME_COUNT), end='')
+    print('')
+
+    average_point = total_point / EN_GAME_COUNT
+    print('AveragePoint', average_point)
+
+    K.clear_session()
+    del model0
+    del model1
+
+    if average_point > 0.5:
+        update_best_player()
+        return True
+    else:
+        return False
+
+
+if __name__ == '__main__':
+    evaluate_network()
 
