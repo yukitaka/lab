68cf828e541df26f9c75652f11e51e682afd18dd
Takayuki Sato <s.yukkiy@gmail.com>
2023-09-09 23:18:56 +0900

    feat: create a empty window

---
+++ machine-learning/example/ai_book7/main.py
@@ -0,0 +1,14 @@
+import tkinter as tk
+
+class EmptyUI(tk.Frame):
+    def __init__(self, master=None):
+        tk.Frame.__init__(self, master)
+        self.master.title('Hello World')
+
+        self.c = tk.Canvas(self, width = 240, height = 240, highlightthickness = 0)
+        self.c.pack()
+
+
+f = EmptyUI()
+f.pack()
+f.mainloop()
