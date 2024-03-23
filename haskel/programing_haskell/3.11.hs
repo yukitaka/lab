c1ddc9375f27b9c1445434575e9b893029ffd08d
Sato Takayuki <s.yukkiy@gmail.com>
2024-03-23 23:31:09 +0900

    feat: 3.11.2

--- haskel/programing_haskell/3.11.hs
+++ haskel/programing_haskell/3.11.hs
@@ -4,3 +4,19 @@
 -- [(False, '0'), (True, '1')] :: [(Bool, Char)]
 -- ([False, True], ['0', '1']) :: ([Bool], [Char])
 -- [tail, init, reverse] :: [[a] -> [a]]
+
+-- 3.11.2
+bools :: [Bool]
+bools = [True, False]
+
+nums :: [[Int]]
+nums = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+
+add :: Int -> Int -> Int -> Int
+add a b c = a + b + c
+
+copy :: a -> (a, a)
+copy a = (a, a)
+
+apply :: (a -> b) -> a -> b
+apply a b = a b
